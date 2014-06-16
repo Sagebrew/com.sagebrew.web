4bc6ff2fd1d0f86959d1fcae0697a086e40483ba
Devon Bleibtrey <bleib1dj@gmail.com>
2014-06-15 23:26:45 -0400

    Added categorization using internal django support and moved logic to utils

--- sagebrew/sb_registration/views.py
+++ sagebrew/sb_registration/views.py
@@ -4,22 +4,19 @@ from django.http import HttpResponseServerError
 from plebs.neo_models import Pleb, TopicCategory, SBTopic
 
 from .forms import InterestForm
+from .utils import generate_interests_tuple
 
 def profile_information(request):
     return render(request, 'profile_info.html',
                     {'profile_information_form':None})
 
+
 def interests(request):
     interest_form = InterestForm(request.POST or None)
 
-    cat_instance = TopicCategory.category()
-    categories = cat_instance.instance.all()
-    specific_interest_choices = []
-    for category in categories:
-        for item in category.sb_topics.all():
-            specific_interest_choices.append((item.title, item.title))
+    choices_tuple = generate_interests_tuple()
+    interest_form.fields["specific_interests"].choices = choices_tuple
 
-    interest_form.fields["specific_interests"].choices = specific_interest_choices
     if interest_form.is_valid():
         for item in interest_form.cleaned_data:
             if(interest_form.cleaned_data[item] and

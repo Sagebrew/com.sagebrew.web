3923d04a5236211ddc7b78ad3bd7e276ff20051a
Jordan Welton <jordan.welton@gmail.com>
2014-07-20 18:34:51 -0400

    Restored some css files

---
+++ sagebrew/static/js/edit_post.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+    $("a.show_edit_post_class").click(function(event){
+        var post_id = $(this).data('uuid');
+        $('#divid_'+post_id).fadeToggle();
+    });
+	$("a.edit_post-action").click(function(event){
+		event.preventDefault();
+		$.ajaxSetup({
+		    beforeSend: function(xhr, settings) {
+				var csrftoken = $.cookie('csrftoken');
+		        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+		            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		        }
+		    }
+		});
+	   	$.ajax({
+			xhrFields: {withCredentials: true},
+			type: "POST",
+			url: "/posts/edit_post/",
+			data: JSON.stringify({
+			   'content':$('textarea#'+$(this).data('post_id')).val(),
+			   'pleb': $(this).data('pleb'),
+               'post_id': $(this).data('post_id')
+			}),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(data) {
+			   alert(data['here']);
+			}
+		});
+	});
+});
+
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
+}

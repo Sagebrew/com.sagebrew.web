08744295853952861556b9d1b17c1976369dc192
Jordan Welton <jordan.welton@gmail.com>
2014-07-23 22:37:10 -0400

    Working on cropping profile pic

--- sagebrew/sb_registration/static/js/profile_picture_form.js
+++ sagebrew/sb_registration/static/js/profile_picture_form.js
@@ -1,7 +1,25 @@
 $( document ).ready(function() {
 
-    jQuery(function($) {
-        $('#target').Jcrop();
+
+    function showCoords(c)
+    {
+        $('#x').val(c.x);
+        $('#y').val(c.y);
+        $('#x2').val(c.x2);
+        $('#y2').val(c.y2);
+        $('#w').val(c.w);
+        $('#h').val(c.h);
+    };
+   // $('#image_preview_id').Jcrop();
+   $("#sample_image").on((function() {
+       $('#image_preview_id').Jcrop({
+           onSelect: showCoords,
+           onChange: showCoords
+       });
     });
 
-};
+   /* $("input#profile_pic_upload").change(function () {
+        $('#myModal').modal('show');
+    });
+    */
+});

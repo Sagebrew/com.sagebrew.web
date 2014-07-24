fe76aa334d32e6f13cbb2ff9230ad63a62144e44
Jordan Welton <jordan.welton@gmail.com>
2014-07-24 00:24:49 -0400

    Began to reorganize sb_registration.css started positioning of profile picture

--- sagebrew/sb_registration/static/js/profile_picture_form.js
+++ sagebrew/sb_registration/static/js/profile_picture_form.js
@@ -1,32 +1,10 @@
 $( document ).ready(function() {
 
-
-    function showCoords(c) {
-        $('#x').val(c.x);
-        $('#y').val(c.y);
-        $('#x2').val(c.x2);
-        $('#y2').val(c.y2);
-        $('#w').val(c.w);
-        $('#h').val(c.h);
-    };
-
-    $( "#sample_image" ).on("click", "img", function() {
-        console.log("here");
+    $( "#image_uploaded" ).on("mouseenter", "img", function() {
         $("img").Jcrop();
     });
-    // $('#image_preview_id').Jcrop();
 
-
-    /*
-     $("input#profile_pic_upload").change(function() {
-         $('#image_preview_id').Jcrop({
-         onSelect: showCoords,
-         onChange: showCoords
-         });
-    });
-
-   ("input#profile_pic_upload").change(function () {
+   $("input#profile_pic_upload").change(function () {
         $('#myModal').modal('show');
     });
-    */
 });

f1320bc1e70bfb544f145fbb090c75cab6f1ef04
Devon Bleibtrey <bleib1dj@gmail.com>
2014-07-24 20:27:52 -0400

    New implementation for select/deselect all checkboxes in interests. Not fully tested but appears to work and be more robust then the previous implementation. It did fix the issue where the checkbox would become unresponsive if you selected select all and then an individual checkbox

--- sagebrew/sb_registration/static/js/interest_form.js
+++ sagebrew/sb_registration/static/js/interest_form.js
@@ -1,44 +1,37 @@
 $( document ).ready(function() {
-
-    $('.toggle-all').click(function() {
-        var toggle_class = $('.toggle-all').attr('class');
-        var lastClass = toggle_class.substr( toggle_class.lastIndexOf(' ') + 1);
-        $('#select_all_checkboxes input').each(function(inde, item) {
-            var label = $("label[for='"+$(item).attr('id')+"']")[1];
-            var label_class = $(label).attr('class');
-            var label_last = label_class.substr( label_class.lastIndexOf(' ') + 1);
-            if(lastClass !== "checked") {
-                if (label_last !== 'checked') {
-                    $(item).val(true);
-                    $(label).addClass('checked');
-                }
-            } else {
-                if (label_last === 'checked'){
-                    $(item).val(false);
-                    $(label).removeClass('checked');
-                }
-            }
-        });
+    /**
+     * This is the function that selects all the fields and deselects all the
+     * fields. It works even if fields are already selected or unselected.
+     * It also assigns the value to the checkbox of true or false with the
+     * end dot notation of .val(!ch).
+     */
+    $('.toggle-all').on('click', function() {
+      var ch = $(this).find(':checkbox').prop('checked');
+      $('#select_all_checkboxes').find('.checkbox-toggle :checkbox').checkbox(!ch ? 'check' : 'uncheck').val(!ch);
     });
 
-    $('#select_all_checkboxes input').each(function(ind, item) {
-        console.log($(item));
+    /**
+     * Does individual checkboxes and when they are clicked assigns the value
+     * associated with the checkbox input to either true or false since
+     * it appears bootstrap/flat ui rely on the class of the label to change
+     * between checked and a blank string rather then the actual value of the
+     * input. This is needed for Django to understand what was selected not
+     * for the actual view of the interface. The actual checkbox population in
+     * the interface is done automatically by flat ui's js files.
+     */
+    $('.checkbox-toggle input').each(function(ind, item) {
        $(item).change(function(){
            var label = $("label[for='"+$(item).attr('id')+"']")[1];
            var label_class = $(label).attr('class');
-           var label_last = label_class.substr( label_class.lastIndexOf(' ') + 1);
-           var state = $(item).val();
-           console.log("here");
-           console.log(state);
+           var label_last = label_class.substr(label_class.lastIndexOf(' ') + 1);
+
            if(label_last === "checked"){
                $(item).val(true);
            } else {
                $(item).val(false);
-               $(label).removeClass('checked');
            }
        });
     });
-
     $("[data-toggle='tooltip']").tooltip('hide');
 
     $("#id_specific_interests").select2({

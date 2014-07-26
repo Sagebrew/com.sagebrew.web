d7ad35508aff1ead12371fc0cf71a958f2e2ddab
Jordan Welton <jordan.welton@gmail.com>
2014-07-25 23:47:47 -0400

    worked on jcrop and profile picturee

--- sagebrew/sb_registration/static/js/profile_picture_form.js
+++ sagebrew/sb_registration/static/js/profile_picture_form.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
     $( "#image_uploaded" ).on("mouseenter", "img", function() {
         $("img").Jcrop({
            aspectRatio: 150/150,
-           bgColor: 'white',
-           bgOpacity: 0.5,
+           bgColor: '#26424a',
+           bgOpacity: 0.4,
 
         });
 

253431742309504f235c28eebe7d518a2c61a8dd
Jordan Welton <jordan.welton@gmail.com>
2014-07-31 15:30:52 -0400

    continuing work on profile picture upload

--- sagebrew/sb_registration/static/js/profile_picture_form.js
+++ sagebrew/sb_registration/static/js/profile_picture_form.js
@@ -1,17 +1,25 @@
 $( document ).ready(function() {
 
+    $("input#profile_pic_upload").change(function () {
+        $("#myModal").modal("show");
+        var existingdiv1 = document.getElementById( "sb_profile_photo" );
+        $("#sb_modal_content").append(existingdiv1);
+        $("#sb_save").show();
+        console.log(existingdiv1);
+        });
+
     $( "#image_uploaded" ).on("mouseenter", "img", function() {
         $("img").Jcrop({
            aspectRatio: 150/150,
            bgColor: '#26424a',
            bgOpacity: 0.4,
-
         });
-
     });
-
-   $("input#profile_pic_upload").change(function () {
-        $('#myModal').modal('show');
-    });
-
 });
+
+/*
+    $("#close").change(function () {
+        var existingdiv2 = document.getElementById( "sb_photo_parent" );
+        $("#sb_profile_photo").append(existingdiv2);
+    });
+*/

c9ce61e96854374f25bdcbae11d05b1b13bde712
tylerwiersing <tyler.wiersing@gmail.com>
2014-08-02 18:40:50 -0400

    added bomberman models

--- sagebrew/api/models.py
+++ sagebrew/api/models.py
@@ -1,1 +1,9 @@
 # Create your models here.
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+class BombermanKeys(TimeStampedModel):
+    bomberman_id = models.IntegerField(primary_key=True)
+    bomberman_key = models.CharField(max_length=1000)
+    count = models.IntegerField(default=0)
+    count_limit = models.IntegerField(default=10)

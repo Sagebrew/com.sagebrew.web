7fc71eea357e61530ef95a4aa9d32321fc655826
Devon Bleibtrey <bleib1dj@gmail.com>
2014-08-02 16:23:06 -0400

    Updated urls

--- sagebrew/sb_registration/urls.py
+++ sagebrew/sb_registration/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, url
 from .views import (profile_information, interests, profile_picture)
 
 
-urlpatterns = patterns('sb_registration.views',
-                       url(r'^profile_information/$', profile_information,
-                           name="profile_info"),
-                       url(r'^interests/$', interests, name="interests"),
-                       url(r'^profile_picture/$', profile_picture,
-                           name="profile_picture")
+urlpatterns = patterns(
+    'sb_registration.views',
+    url(r'^profile_information/$', profile_information, name="profile_info"),
+    url(r'^interests/$', interests, name="interests"),
+    url(r'^profile_picture/$', profile_picture, name="profile_picture")
 )
--- sagebrew/sb_relationships/urls.py
+++ sagebrew/sb_relationships/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, url
 from .views import get_friend_requests, create_friend_request, \
     respond_friend_request
 
-urlpatterns = patterns('sb_relationships.views',
-                       url(r'^query_friend_requests/$', get_friend_requests,
-                           name="get_friend_requests"),
-                       url(r'^create_friend_request/$', create_friend_request,
-                           name="create_friend_request"),
-                       url(r'^respond_friend_request/$',
-                           respond_friend_request,
-                           name="respond_friend_request")
+urlpatterns = patterns(
+    'sb_relationships.views',
+    url(r'^query_friend_requests/$', get_friend_requests,
+       name="get_friend_requests"),
+    url(r'^create_friend_request/$', create_friend_request,
+       name="create_friend_request"),
+    url(r'^respond_friend_request/$', respond_friend_request,
+       name="respond_friend_request")
 )

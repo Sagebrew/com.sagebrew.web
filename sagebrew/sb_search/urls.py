0f96b6b60a5c6cf08e6b87192d1135b59e13e66b
tylerwiersing <tyler.wiersing@gmail.com>
2014-08-09 17:49:19 -0400

    added sb_search began implementing search

--- sagebrew/sb_search/urls.py
+++ sagebrew/sb_search/urls.py
@@ -5,5 +5,5 @@ from .views import search_result_view, search_view
 urlpatterns = patterns(
     'sb_search.views',
     url(r'^$', search_view, name="search"),
-    url(r'^results/q=(?P<query_param>a-zA-Z0-9$', search_result_view, name="search_results"),
+    url(r'^results/q=(?P<query_param>a-zA-Z0-9$)', search_result_view, name="search_results"),
 )

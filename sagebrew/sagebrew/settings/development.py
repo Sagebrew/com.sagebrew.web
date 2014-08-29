e1e86e946bb1e73c3aefd7097bf0a4fb386a2c7d
Devon Bleibtrey <bleib1dj@gmail.com>
2014-08-29 19:20:38 -0400

    Working with circle.yml file

--- sagebrew/sagebrew/settings/development.py
+++ sagebrew/sagebrew/settings/development.py
@@ -96,7 +96,7 @@ DEBUG_TOOLBAR_CONFIG = {
     'ENABLE_STACKTRACES': True,
 }
 
-INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar', )
+#INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar', )
 # MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
 # 'debug_toolbar.middleware.DebugToolbarMiddleware',)
 

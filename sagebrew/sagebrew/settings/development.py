7b58a91d0b0066151ca754dae85ca75ff6a37928
Devon Bleibtrey <bleib1dj@gmail.com>
2014-08-29 21:06:13 -0400

    Trying to fix rabbitmq issues with circle

--- sagebrew/sagebrew/settings/development.py
+++ sagebrew/sagebrew/settings/development.py
@@ -15,6 +15,10 @@ API_PASSWORD = "admin"
 
 VERIFY_SECURE = False
 
+# TODO this makes it so we cannot run tests concurrently (parallel processing in
+# circle. This is because the test db gets created on this server and then
+# if another one starts running it fails. Might want to look into making a
+# separate test config again that uses a local psql db based on circles docs.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -44,7 +48,7 @@ AWS_BUCKET_NAME = "sagebrew"
 AWS_ACCESS_KEY_ID = "AKIAJIWX3E2JPTBS6CRA"
 AWS_SECRET_ACCESS_KEY = "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"
 AWS_PROFILE_PICTURE_FOLDER_NAME = 'profile_pictures'
-
+BROKER_URL = 'amqp://localhost:5672//'
 SECRET_KEY = "5fd&2wkqx8r!h2y1)j!izqi!982$p87)sred(5#x0mtqa^cbx)"
 
 INTERNAL_IPS = ('127.0.0.1', 'localhost', '0.0.0.0', '192.168.56.101')

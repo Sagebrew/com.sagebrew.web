0fd339fbf48c5d03161b6f0fe6e73bd4884bfea7
Devon Bleibtrey <bleib1dj@gmail.com>
2014-09-11 21:18:15 -0400

    Added logic for managing circle builds and docker configuration to the circle config

--- sagebrew/api/management/commands/dockerfilepopulation.py
+++ sagebrew/api/management/commands/dockerfilepopulation.py
@@ -10,14 +10,36 @@ class Command(BaseCommand):
     def populate_dockerfiles(self):
         with open ("%s/dockerfile_template" % settings.REPO_DIR,
                    "r") as dockerfile:
-            data = dockerfile.read().replace('{{DOCKER_ENV}}', os.environ.get(
-                "DOCKER_ENV", ""))
+            circle_branch = os.environ.get("CIRCLE_BRANCH", None)
+            data = dockerfile.read()
+            if(circle_branch is not None):
+                project_username = os.environ.get("CIRCLE_PROJECT_USERNAME", "")
+                project_reponame = os.environ.get("CIRCLE_PROJECT_REPONAME", "")
+                data = data.replace(
+                    '{{DOCKER_ENV}}', os.environ.get("CIRCLE_BRANCH", ""))
+                data = data.replace("{{PROJECT_REPONAME}}", project_reponame)
+                data = data.replace("{{PROJECT_USERNAME}}", project_username)
+                if(circle_branch == "staging"):
+                    data = data.replace('{{REQUIREMENTS_FILE}}', "production")
+                elif("dev" in circle_branch):
+                    data = data.replace('{{REQUIREMENTS_FILE}}', "test")
+                elif(circle_branch == "master"):
+                    data = data.replace('{{REQUIREMENTS_FILE}}', "production")
+            else:
+                data = data.replace(
+                    '{{DOCKER_ENV}}', os.environ.get("BRANCH", ""))
+                data = data.replace('{{REQUIREMENTS_FILE}}',
+                                    os.environ.get(
+                                        "REQUIREMENTS_FILE", "production"))
+                data = data.replace("{{PROJECT_REPONAME}}",
+                                    os.environ.get("PROJECT_REPONAME", ""))
+                data = data.replace("{{PROJECT_USERNAME}}",
+                                    os.environ.get("PROJECT_USERNAME", ""))
             data = data.replace('{{APP_USER}}', os.environ.get(
                 "APP_USER", ""))
-            data = data.replace('{{REQUIREMENTS_FILE}}', os.environ.get(
-                "REQUIREMENTS_FILE", ""))
             data = data.replace('{{BOMBERMAN_API_KEY}}', os.environ.get(
                 "BOMBERMAN_API_KEY", ""))
+            data = data.replace("{{PROJECT_NAME}}", "sagebrew")
             web_docker = data.replace('{{SUPER_TEMPLATE}}', "web")
             worker_docker = data.replace('{{SUPER_TEMPLATE}}', "worker")
         f = open("%s/dockerfiles/web_app/Dockerfile" % settings.REPO_DIR, "w")

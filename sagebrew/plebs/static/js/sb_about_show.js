ea7ec1fdce9b9551e114160ca5c302d5ce792f3e
Jordan Welton <jordan.welton@gmail.com>
2014-09-20 14:23:49 -0400

    profie

--- sagebrew/plebs/static/js/sb_about_show.js
+++ sagebrew/plebs/static/js/sb_about_show.js
@@ -1,9 +1,3 @@
 $( document ).ready(function(){
-    $("#sb_btn_about").click(function(){
-        $(".sb_about").show();
-        $(".sb_home").hide();
-    });
-    $("sb_btn_home").click(function(){
-       $(".sb_about").hide();
-    });
+
 });

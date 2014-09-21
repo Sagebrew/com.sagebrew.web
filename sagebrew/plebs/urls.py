32775c7875d67adb03c000ed7653654eeae62cb8
Jordan Welton <jordan.welton@gmail.com>
2014-09-21 11:49:08 -0400

    profie

--- sagebrew/plebs/urls.py
+++ sagebrew/plebs/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, url
 
-from .views import profile_page, get_user_search_view
+from .views import (profile_page, get_user_search_view,
+                    about_page, friends_page, reputation_page)
 
 
 urlpatterns = patterns(
     'plebs.views',
     url(r'^search/(?P<pleb_email>[A-Za-z0-9.@_%+-]{1,40})', get_user_search_view,
         name="get_user_search_view"),
+    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/about/',
+       about_page, name="about_page"),
+    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/friends/',
+       friends_page, name="friends"),
+    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/reputation/',
+       reputation_page, name="reputation"),
     url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/',
        profile_page, name="profile_page"),
 

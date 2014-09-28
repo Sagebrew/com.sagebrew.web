ea52bf20ab968cf3a56bdaf04a52c7b5bb22d7ac
tylerwiersing <tyler.wiersing@gmail.com>
2014-09-28 15:00:00 -0400

    removed plebs.models and proxy user model

--- sagebrew/plebs/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-
-class ProxyUser(User):
-    class Meta:
-        proxy = True
-        permissions = (
-            ("has_registered", "Can use the full site")
-        )

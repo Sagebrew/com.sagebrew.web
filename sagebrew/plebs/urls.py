7bdc23c116a10b416ca746c3bb84c3757db4610a
Jordan Welton <jordan.welton@gmail.com>
2014-10-05 14:55:33 -0400

    updated pleb urls to reflect Tylers's changes

--- sagebrew/plebs/urls.py
+++ sagebrew/plebs/urls.py
@@ -1,19 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import (profile_page, get_user_search_view,
-                    about_page, friends_page, reputation_page)
+from .views import profile_page, get_user_search_view
 
 
 urlpatterns = patterns(
     'plebs.views',
     url(r'^search/(?P<pleb_email>[A-Za-z0-9.@_%+-]{1,40})', get_user_search_view,
         name="get_user_search_view"),
-    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/about/',
-       about_page, name="about_page"),
-    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/friends/',
-       friends_page, name="friends_page"),
-    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/reputation/',
-       reputation_page, name="reputation_page"),
     url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/',
        profile_page, name="profile_page"),
 

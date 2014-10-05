957d5c45081b755f788ac033b3efe8f3e823ef71
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-05 17:04:18 -0400

    Reordering pleb urls

--- sagebrew/plebs/urls.py
+++ sagebrew/plebs/urls.py
@@ -7,8 +7,7 @@ from .views import (profile_page, get_user_search_view,
 
 urlpatterns = patterns(
     'plebs.views',
-    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/',
-       profile_page, name="profile_page"),
+
     url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/about/',
        about_page, name="about_page"),
     url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/friends/',
@@ -17,5 +16,7 @@ urlpatterns = patterns(
        reputation_page, name="reputation_page"),
     url(r'^search/(?P<pleb_email>[A-Za-z0-9.@_%+-]{1,40})',
         get_user_search_view, name="get_user_search_view"),
+    url(r'^(?P<pleb_email>[A-Za-z0-9.@_%+-]{7,40})/',
+       profile_page, name="profile_page"),
 
 )

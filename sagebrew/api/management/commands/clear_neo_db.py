5bb7915dd63fbfdd932933d3abdd5a3eee309114
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-11 19:37:11 -0400

    Initial tests of management commands

--- sagebrew/api/management/commands/clear_neo_db.py
+++ sagebrew/api/management/commands/clear_neo_db.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from neomodel import db
 from neomodel.exception import CypherException
 
+
 class Command(BaseCommand):
     args = "None."
 
@@ -17,8 +18,8 @@ class Command(BaseCommand):
                     res = db.cypher_query(
                         "START n=node(*) OPTIONAL MATCH n-[r]-() DELETE r,n")
             except CypherException:
-                pass
+                return False
+        return True
 
     def handle(self, *args, **options):
         self.clear_neo_db()
-
---
+++ sagebrew/api/tests/test_management_commands.py
@@ -0,0 +1,26 @@
+import pytz
+import time
+from uuid import uuid1
+from datetime import datetime
+from django.test import TestCase
+from django.core.management import call_command
+from django.contrib.auth.models import User
+
+from plebs.neo_models import Pleb
+
+
+class TestSaveAnswerTask(TestCase):
+
+    def test_clear_neo_db(self):
+        email = 'devon@sagebrew.com'
+        user = User.objects.create_user(
+            username='Tyler' + str(uuid1())[:25], email=email)
+        pleb = Pleb.nodes.get(email=email)
+        self.assertEqual(pleb.email, user.email)
+        call_command("clear_neo_db")
+        pleb = None
+        try:
+            pleb = Pleb.nodes.get(email=email)
+            self.assertIsNone(pleb)
+        except Pleb.DoesNotExist:
+            self.assertIsNone(pleb)

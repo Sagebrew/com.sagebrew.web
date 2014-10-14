3866698831970f30aac515e26b9e202a3c7ee8d7
tylerwiersing <tyler.wiersing@gmail.com>
2014-10-14 17:22:55 -0400

    added logging support for elasticsearch

--- sagebrew/sagebrew/settings/development.py
+++ sagebrew/sagebrew/settings/development.py
@@ -145,6 +145,13 @@ LOGGING = {
             'level': 'DEBUG',
             'token': LOG_TOKEN
         },
+        'elasticsearch': {
+            'handlers': ['logging.handlers.SysLogHandler'],
+            'propagate': True,
+            'format': 'loggly: %(message)s',
+            'level': 'DEBUG',
+            'token': LOG_TOKEN
+        },
         'django.request': {
             'handlers': ['logging.handlers.SysLogHandler'],
             'level': 'ERROR',

de8455978a420b6fd4fdd5d661acccf17e7601b3
tylerwiersing <tyler.wiersing@gmail.com>
2014-10-16 17:16:59 -0400

    updated some pleb tasks

--- sagebrew/plebs/tasks.py
+++ sagebrew/plebs/tasks.py
@@ -4,6 +4,7 @@ from logging import getLogger
 from django.conf import settings
 from django.template.loader import get_template
 from django.template import Context
+from neomodel import DoesNotExist
 
 from .neo_models import Pleb
 from api.utils import spawn_task
@@ -19,46 +20,56 @@ token_gen = EmailAuthTokenGenerator()
 @shared_task()
 def create_pleb_task(user_instance):
     try:
-        pleb = Pleb(email=user_instance.email,
-                    first_name=user_instance.first_name,
-                    last_name=user_instance.last_name)
-        pleb.save()
-        wall = SBWall(wall_id=uuid1())
-        wall.save()
-        wall.owner.connect(pleb)
-        pleb.wall.connect(pleb)
-        wall.save()
-        pleb.save()
+        try:
+            test = Pleb.nodes.get(email=user_instance.email)
+            return False
+        except (Pleb.DoesNotExist, DoesNotExist):
+            pleb = Pleb(email=user_instance.email,
+                        first_name=user_instance.first_name,
+                        last_name=user_instance.last_name)
+            pleb.save()
+            wall = SBWall(wall_id=uuid1())
+            wall.save()
+            wall.owner.connect(pleb)
+            pleb.wall.connect(pleb)
+            wall.save()
+            pleb.save()
 
-        task_data = {'object_data': {
-                'first_name': pleb.first_name,
-                'last_name': pleb.last_name,
-                'full_name': str(pleb.first_name) + ' '
-                             + str(pleb.last_name),
-                'pleb_email': pleb.email
-                },
-                         'object_type': 'pleb'
-            }
-        spawn_task(task_func=add_object_to_search_index,
+            task_data = {'object_data': {
+                    'first_name': pleb.first_name,
+                    'last_name': pleb.last_name,
+                    'full_name': str(pleb.first_name) + ' '
+                                 + str(pleb.last_name),
+                    'pleb_email': pleb.email
+                    },
+                             'object_type': 'pleb'
+                }
+            spawn_task(task_func=add_object_to_search_index,
+                           task_param=task_data)
+            task_data = {'pleb': pleb.email,
+                         'index': "full-search-user-specific-1"}
+            spawn_task(task_func=add_user_to_custom_index,
                        task_param=task_data)
-        task_data = {'pleb': pleb.email,
-                     'index': "full-search-user-specific-1"}
-        spawn_task(task_func=add_user_to_custom_index,
-                   task_param=task_data)
-        template_dict = {
-            'full_name': pleb.first_name+' '+pleb.last_name,
-            'verification_url': settings.EMAIL_VERIFICATION_URL+token_gen.make_token(user_instance)+'/'
-        }
-        subject, to = "Sagebrew Email Verification", pleb.email
-        text_content = get_template('email_templates/email_verification.txt').render(Context(template_dict))
-        html_content = get_template('email_templates/email_verification.html').render(Context(template_dict))
-        sb_send_email(to, subject, text_content, html_content)
-        return True
+            template_dict = {
+                'full_name': pleb.first_name+' '+pleb.last_name,
+                'verification_url': settings.EMAIL_VERIFICATION_URL+token_gen.make_token(user_instance)+'/'
+            }
+            subject, to = "Sagebrew Email Verification", pleb.email
+            text_content = get_template('email_templates/email_verification.txt').render(Context(template_dict))
+            html_content = get_template('email_templates/email_verification.html').render(Context(template_dict))
+            sb_send_email(to, subject, text_content, html_content)
+            return True
     except Exception:
         logger.exception({"function": create_pleb_task.__name__,
                           "exception": "UnhandledException: "})
         raise create_pleb_task.retry(exc=Exception, countdown=3,
                                      max_retries=None)
 
-def send_email_task():
-    pass
+@shared_task()
+def send_email_task(to, subject, text_content, html_content):
+    try:
+        sb_send_email(to, subject, text_content, html_content)
+    except Exception:
+        logger.exception({"function": send_email_task.__name__,
+                          "exception": "UnhandledException: "})
+        raise send_email_task.retry(exc=Exception, countdown=3, max_retries=None)

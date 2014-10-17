46ec030b0b8543076cc59bcfa2ae5e2e70291cc2
tylerwiersing <tyler.wiersing@gmail.com>
2014-10-17 12:54:16 -0400

    working on test failure

--- sagebrew/sb_questions/utils.py
+++ sagebrew/sb_questions/utils.py
@@ -2,7 +2,7 @@ import logging
 from uuid import uuid1
 from textblob import TextBlob
 
-from neomodel import DoesNotExist
+from neomodel import DoesNotExist, UniqueProperty
 from django.conf import settings
 from django.template.loader import render_to_string
 
@@ -66,6 +66,8 @@ def create_question_util(content="", current_pleb="", question_title="",
         tag_list = {'tag_list': task_data}
         spawn_task(task_func=add_auto_tags, task_param=tag_list)
         return my_question
+    except UniqueProperty:
+        return False
     except Exception:
         logger.exception({"function": create_question_util.__name__,
                           'exception': "UnhandledException: "})

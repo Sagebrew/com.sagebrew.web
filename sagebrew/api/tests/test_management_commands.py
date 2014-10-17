daa7f622089f5439065678717e8da394fda04afe
tylerwiersing <tyler.wiersing@gmail.com>
2014-10-17 19:35:34 -0400

    updated tests

--- sagebrew/api/tests/test_management_commands.py
+++ sagebrew/api/tests/test_management_commands.py
@@ -1,18 +1,29 @@
+import time
 from uuid import uuid1
 from django.test import TestCase
 from django.core.management import call_command
 from django.contrib.auth.models import User
 
 from plebs.neo_models import Pleb
+from sb_registration.utils import create_user_util
 
 
 class TestClearNeoDBCommand(TestCase):
 
     def test_clear_neo_db(self):
-        email = 'devon@sagebrew.com'
-        user = User.objects.create_user(
-            username='Tyler' + str(uuid1())[:25], email=email)
-        pleb = Pleb.nodes.get(email=email)
+        email = "success@simulator.amazonses.com"
+        res = create_user_util("test", "test", email, "testpassword")
+        while not res['task_id'].ready():
+            time.sleep(1)
+        self.assertTrue(res['task_id'].result)
+        while True:
+            try:
+                pleb = Pleb.nodes.get(email= email)
+                user = User.objects.get(email=email)
+            except Exception:
+                pass
+            else:
+                break
         self.assertEqual(pleb.email, user.email)
         call_command("clear_neo_db")
         pleb = None

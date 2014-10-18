749504d9c25049acf74b2b2fe2f627e72c5360df
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-18 17:06:00 -0400

    Updated settings loads based on hostname

--- sagebrew/sagebrew/settings/__init__.py
+++ sagebrew/sagebrew/settings/__init__.py
@@ -1,11 +1,11 @@
 import socket
 
 hostname = socket.gethostname()
-if hostname == 'sagebrew':
-    from production import *
+if 'kepric' in hostname:
+    from development import *
 elif hostname == 'staging-sagebrew':
     from staging import *
 if('box' in hostname):
     from test import *
 else:
-    from development import *
+    from production import *

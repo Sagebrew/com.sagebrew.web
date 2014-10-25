9808fb5ef57cad453c22f4ba2c970418b1e1ee79
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-25 18:29:13 -0400

    Updated master script

--- deployment/master.sh
+++ deployment/master.sh
@@ -3,11 +3,11 @@
 SHA1=$1
 docker push sagebrew/sb_web:$SHA1
 docker push sagebrew/sb_worker:$SHA1
-EB_BUCKET=sagebrew-$CIRCLE_BRANCH/$DOCKER_BUCKET
+EB_BUCKET=sagebrew-$CIRCLE_BRANCH/$DOCKER_CONFIG_BUCKET
 DOCKERRUN_FILE_WEB=$SHA1-master_Docker_web.aws.json
 DOCKERRUN_FILE_WORKER=$SHA1-master_Docker_worker.aws.json
-sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_worker/;" < aws_templates/Dockerrun.aws.json.worker_template > $DOCKERRUN_FILE_WORKER
-sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_web/;" < aws_templates/Dockerrun.aws.json.web_template > $DOCKERRUN_FILE_WEB
+sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_worker/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.worker_template > $DOCKERRUN_FILE_WORKER
+sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_web/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.web_template > $DOCKERRUN_FILE_WEB
 
 aws s3 cp $DOCKERRUN_FILE_WEB s3://$EB_BUCKET/$DOCKERRUN_FILE_WEB
 

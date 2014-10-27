26a61ffc211f38043454a345016904d95e98d3ff
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-27 16:16:13 -0400

    Working on populating environmental variables correctly

--- deployment/staging.sh
+++ deployment/staging.sh
@@ -11,70 +11,17 @@ DOCKERRUN_WORKER_ENVIRONMENT=$SHA1-staging_Docker_worker_environment.txt
 
 sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_worker/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.worker_template > $DOCKERRUN_FILE_WORKER
 sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_web/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.web_template > $DOCKERRUN_FILE_WEB
-
-sed "s/<APP_USER>/$APP_USER/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<REPO_NAME>/$REPO_NAME/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<PROJECT_NAME>/$PROJECT_NAME/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<LOG_ACCOUNT>/$LOG_ACCOUNT/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<LOG_TOKEN>/$LOG_TOKEN/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<CIRCLE_BRANCH>/$CIRCLE_BRANCH/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<APPLICATION_SECRET_KEY>/$APPLICATION_SECRET_KEY/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<BOMBERMAN_API_KEY>/$BOMBERMAN_API_KEY/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<SSL_CERT_LOCATION>/$SSL_CERT_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<SSL_KEY_LOCATION>/$SSL_KEY_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<AWS_S3_BUCKET>/$AWS_S3_BUCKET/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ALCHEMY_API_KEY>/$ALCHEMY_API_KEY/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ADDRESS_VALIDATION_ID>/$ADDRESS_VALIDATION_ID/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ADDRESS_VALIDATION_TOKEN>/$ADDRESS_VALIDATION_TOKEN/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_HOST>/$ELASTIC_SEARCH_HOST/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_PORT>/$ELASTIC_SEARCH_PORT/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_USER>/$ELASTIC_SEARCH_USER/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_KEY>/$ELASTIC_SEARCH_KEY/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<REDIS_LOCATION>/$REDIS_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<REDIS_PORT>/$REDIS_PORT/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<QUEUE_USERNAME>/$QUEUE_USERNAME/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<QUEUE_PASSWORD>/$QUEUE_PASSWORD/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<QUEUE_HOST>/$QUEUE_HOST/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-sed "s/<QUEUE_PORT>/$QUEUE_PORT/;" < ~/com.sagebrew.web/aws_environment_config/web_environment_template.config > $DOCKERRUN_WEB_ENVIRONMENT
-
-
-sed "s/<APP_USER>/$APP_USER/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<REPO_NAME>/$REPO_NAME/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<PROJECT_NAME>/$PROJECT_NAME/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<LOG_ACCOUNT>/$LOG_ACCOUNT/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<LOG_TOKEN>/$LOG_TOKEN/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<CIRCLE_BRANCH>/$CIRCLE_BRANCH/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<APPLICATION_SECRET_KEY>/$APPLICATION_SECRET_KEY/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<BOMBERMAN_API_KEY>/$BOMBERMAN_API_KEY/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<SSL_CERT_LOCATION>/$SSL_CERT_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<SSL_KEY_LOCATION>/$SSL_KEY_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<AWS_S3_BUCKET>/$AWS_S3_BUCKET/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ALCHEMY_API_KEY>/$ALCHEMY_API_KEY/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ADDRESS_VALIDATION_ID>/$ADDRESS_VALIDATION_ID/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ADDRESS_VALIDATION_TOKEN>/$ADDRESS_VALIDATION_TOKEN/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_HOST>/$ELASTIC_SEARCH_HOST/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_PORT>/$ELASTIC_SEARCH_PORT/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_USER>/$ELASTIC_SEARCH_USER/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<ELASTIC_SEARCH_KEY>/$ELASTIC_SEARCH_KEY/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<REDIS_LOCATION>/$REDIS_LOCATION/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<REDIS_PORT>/$REDIS_PORT/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<QUEUE_USERNAME>/$QUEUE_USERNAME/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<QUEUE_PASSWORD>/$QUEUE_PASSWORD/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<QUEUE_HOST>/$QUEUE_HOST/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-sed "s/<QUEUE_PORT>/$QUEUE_PORT/;" < ~/com.sagebrew.web/aws_environment_config/worker_environment_template.config > $DOCKERRUN_WORKER_ENVIRONMENT
-
-
+~/virtualenvs/venv-system/bin/python ~/com.sagebrew.web/sagebrew/manage.py populateenvconfig
+cat $DOCKERRUN_FILE_WEB
 
 aws s3 cp $DOCKERRUN_FILE_WEB s3://$EB_BUCKET/$DOCKERRUN_FILE_WEB
 
 #/home/ubuntu/AWS-ElasticBeanstalk-CLI-2.6.3/eb/linux/python2.7/eb
 
-aws elasticbeanstalk create-application-version --application-name staging-web \
+aws elasticbeanstalk create-application-version --application-name sagebrew-staging-web \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE_WEB
 
-aws elasticbeanstalk update-environment --environment-name sagebrew-staging \
+aws elasticbeanstalk update-environment --environment-name sagebrew-staging-web \
     --version-label $SHA1 -f $DOCKERRUN_WEB_ENVIRONMENT
 
 
@@ -84,7 +31,7 @@ aws s3 cp $DOCKERRUN_FILE_WORKER s3://$EB_BUCKET/$DOCKERRUN_FILE_WORKER
 
 #/home/ubuntu/AWS-ElasticBeanstalk-CLI-2.6.3/eb/linux/python2.7/eb
 
-aws elasticbeanstalk create-application-version --application-name staging-worker \
+aws elasticbeanstalk create-application-version --application-name sagebrew-staging-worker \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE_WORKER
 
 aws elasticbeanstalk update-environment --environment-name sagebrew-staging-worker \
---
+++ sagebrew/api/management/commands/populateenvconfig.py
@@ -0,0 +1,75 @@
+from os import environ
+import logging
+import multiprocessing
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+logger = logging.getLogger('loggly_logs')
+
+class Command(BaseCommand):
+
+    def populate_config(self):
+        worker_count = (multiprocessing.cpu_count() *2) + 1
+        if worker_count > 12 and environ.get("CIRCLECI", False):
+            worker_count = 12
+        worker_count = str(worker_count)
+        with open("%s/aws_environment_config/base.conf" % (
+                settings.REPO_DIR), "r") as dockerfile:
+            data = dockerfile.read()
+
+            data = populate_general_values(data)
+        f = open("%s/%s" % (settings.REPO_DIR,
+                            environ.get("DOCKERRUN_WEB_ENVIRONMENT")), "w")
+        f.write(data)
+        f.close()
+        f = open("%s/%s" % (settings.REPO_DIR,
+                            environ.get("DOCKERRUN_WORKER_ENVIRONMENT")), "w")
+        f.write(data)
+        f.close()
+
+
+    def handle(self, *args, **options):
+        self.populate_config()
+
+
+def populate_general_values(data):
+    data = data.replace("<APP_USER>", environ.get("APP_USER", ""))
+    data = data.replace("<REPO_NAME>", environ.get("REPO_NAME", ""))
+    data = data.replace("<PROJECT_NAME>", environ.get("PROJECT_NAME"))
+    data = data.replace("<LOG_ACCOUNT>", environ.get("LOG_ACCOUNT", ""))
+    data = data.replace("<LOG_TOKEN>", environ.get("LOG_TOKEN", ""))
+    data = data.replace("<STAGING_NEO4J_REST_URL>",
+                             environ.get("STAGING_NEO4J_REST_URL", ""))
+    data = data.replace("<CIRCLE_BRANCH>", environ.get("CIRCLE_BRANCH", ""))
+    data = data.replace("<APPLICATION_SECRET_KEY>",
+                        environ.get("APPLICATION_SECRET_KEY", ""))
+    data = data.replace("<BOMBERMAN_API_KEY>",
+                        environ.get("BOMBERMAN_API_KEY", ""))
+    data = data.replace("<SSL_CERT_LOCATION>",
+                        environ.get("SSL_CERT_LOCATION", ""))
+    data = data.replace("<SSL_KEY_LOCATION>", environ.get("SSL_KEY_LOCATION", ""))
+    data = data.replace("<AWS_S3_BUCKET>", environ.get("AWS_S3_BUCKET", ""))
+    data = data.replace("<AWS_SECRET_ACCESS_KEY>",
+                        environ.get("AWS_SECRET_ACCESS_KEY", ""))
+    data = data.replace("<ALCHEMY_API_KEY>", environ.get("ALCHEMY_API_KEY", ""))
+    data = data.replace("<ADDRESS_VALIDATION_ID>",
+                        environ.get("ADDRESS_VALIDATION_ID", ""))
+    data = data.replace("<ADDRESS_VALIDATION_TOKEN>",
+                        environ.get("ADDRESS_VALIDATION_TOKEN", ""))
+    data = data.replace("<ELASTIC_SEARCH_HOST>",
+                        environ.get("ELASTIC_SEARCH_HOST", ""))
+    data = data.replace("<ELASTIC_SEARCH_PORT>",
+                        environ.get("ELASTIC_SEARCH_PORT", ""))
+    data = data.replace("<ELASTIC_SEARCH_USER>",
+                        environ.get("ELASTIC_SEARCH_USER", ""))
+    data = data.replace("<ELASTIC_SEARCH_KEY>",
+                        environ.get("ELASTIC_SEARCH_KEY", ""))
+    data = data.replace("<REDIS_LOCATION>", environ.get("REDIS_LOCATION", ""))
+    data = data.replace("<REDIS_PORT>", environ.get("REDIS_PORT", ""))
+    data = data.replace("<QUEUE_USERNAME>", environ.get("QUEUE_USERNAME", ""))
+    data = data.replace("<QUEUE_PASSWORD>", environ.get("QUEUE_PASSWORD", ""))
+    data = data.replace("<QUEUE_HOST>", environ.get("QUEUE_HOST", ""))
+    data = data.replace("<QUEUE_PORT>", environ.get("QUEUE_PORT", ""))
+
+    return data

26fdd3faac3976008c318166c170dd869eca3d11
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-27 19:19:59 -0400

    Trying to fix bucket issue

--- deployment/staging.sh
+++ deployment/staging.sh
@@ -3,7 +3,7 @@
 SHA1=$1
 docker push sagebrew/sb_web:$SHA1
 #docker push sagebrew/sb_worker:$SHA1
-EB_BUCKET=sagebrew-$CIRCLE_BRANCH/$DOCKER_CONFIG_BUCKET
+EB_BUCKET=sagebrew-$CIRCLE_BRANCH
 DOCKERRUN_FILE_WEB=$SHA1-staging_Docker_web.aws.json
 DOCKERRUN_FILE_WORKER=$SHA1-staging_Docker_worker.aws.json
 DOCKERRUN_WEB_ENVIRONMENT=/home/ubuntu/com.sagebrew.web/$SHA1-staging_Docker_web_environment.json
@@ -14,12 +14,12 @@ sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s
 ~/virtualenvs/venv-system/bin/python ~/com.sagebrew.web/sagebrew/manage.py populateenvconfig $DOCKERRUN_WEB_ENVIRONMENT $DOCKERRUN_WORKER_ENVIRONMENT
 
 
-aws s3 cp $DOCKERRUN_FILE_WEB s3://$EB_BUCKET/$DOCKERRUN_FILE_WEB
+aws s3 cp $DOCKERRUN_FILE_WEB s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$DOCKERRUN_FILE_WEB
 
 #/home/ubuntu/AWS-ElasticBeanstalk-CLI-2.6.3/eb/linux/python2.7/eb
 
 aws elasticbeanstalk create-application-version --application-name sagebrew-staging-web \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE_WEB
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKER_CONFIG_BUCKET/$DOCKERRUN_FILE_WEB
 
 aws elasticbeanstalk update-environment --option-settings file://$DOCKERRUN_WEB_ENVIRONMENT --environment-name sagebrew-staging-web \
     --version-label $SHA1

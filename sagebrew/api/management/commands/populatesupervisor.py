b2dbad03e65830c533b8d3417738e31628987e08
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-29 13:24:58 -0400

    Removing logger of envs

--- sagebrew/api/management/commands/populatesupervisor.py
+++ sagebrew/api/management/commands/populatesupervisor.py
@@ -42,9 +42,7 @@ class Command(BaseCommand):
             f.close()
         else:
             pass
-        logger.critical(dumps({"environment_variables": environ.__dict__,
-                               "host": socket.gethostname(),
-                               "supervisor_conf": env}))
+
     def handle(self, *args, **options):
         self.populate_supervisor(args[0], args[1])
 

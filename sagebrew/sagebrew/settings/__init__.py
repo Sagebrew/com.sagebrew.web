5a21f868bbbe0350f488cd9838efa1ad036b28ec
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-30 18:34:45 -0400

    Checking with prints

--- sagebrew/sagebrew/settings/__init__.py
+++ sagebrew/sagebrew/settings/__init__.py
@@ -7,16 +7,23 @@ if circle_ci == "false":
     circle_ci = False
 if circle_ci == "true":
     circle_ci = True
+print circle_ci
 
 if(circle_ci is True):
+    print "here"
     from test import *
 elif branch is None:
+    print "branch None"
     from production import *
 elif "dev" in branch:
+    print "dev"
     from development import *
 elif branch == "staging":
+    print "branch staging"
     from staging import *
 elif branch == "master":
+    print "branch master"
     from production import *
 else:
+    print "else"
     from production import *

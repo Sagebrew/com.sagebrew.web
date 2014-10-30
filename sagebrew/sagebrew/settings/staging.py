023f6e78e6e35e3f611a26c7a4ed727b0a2c1c18
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-29 20:06:52 -0400

    Adding aws url to list of acceptable hosts for staging

--- sagebrew/sagebrew/settings/staging.py
+++ sagebrew/sagebrew/settings/staging.py
@@ -1,6 +1,7 @@
 from production import *
 
-ALLOWED_HOSTS = ['staging.sagebrew.com', 'staging-web.sagebrew.com']
+ALLOWED_HOSTS = ['staging.sagebrew.com', 'staging-web.sagebrew.com',
+                 'sb-staging-web.elasticbeanstalk.com']
 
 WEB_ADDRESS = "https://staging.sagebrew.com"
 EMAIL_VERIFICATION_URL = "%s/registration/email_confirmation/" % WEB_ADDRESS

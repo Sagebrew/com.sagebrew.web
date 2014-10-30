a777a5a4ee9e9f38f78f67a9ce9b87ad52cf77a6
Devon Bleibtrey <bleib1dj@gmail.com>
2014-10-30 17:59:26 -0400

    Still working on fixing timeout error

--- sagebrew/sagebrew/settings/__init__.py
+++ sagebrew/sagebrew/settings/__init__.py
@@ -1,7 +1,9 @@
 import os
+from socket import gethostname
 
 branch = os.environ.get("CIRCLE_BRANCH", None)
 circle_ci = os.environ.get("CIRCLECI", False)
+hostname = gethostname()
 if circle_ci == "false":
     circle_ci = False
 if(circle_ci):

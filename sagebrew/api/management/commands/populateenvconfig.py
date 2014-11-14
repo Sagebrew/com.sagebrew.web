bebd1734b9b9dbfed8d0fe0e7a3bf71dc105c42d
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-13 19:02:26 -0500

    Removing hardcoded staging values

--- sagebrew/api/management/commands/populateenvconfig.py
+++ sagebrew/api/management/commands/populateenvconfig.py
@@ -46,10 +46,10 @@ class Command(BaseCommand):
             else:
                 data_worker = populate_test_values(data_worker)
             data_worker = populate_general_values(data_worker)
-        sys_env = "/home/ubuntu/com.sagebrew.web/%s-staging_sys_util.json" % (
-            environ.get("CIRCLE_SHA1", "")
+        sys_env = "/home/ubuntu/com.sagebrew.web/%s-%s_sys_util.json" % (
+            environ.get("CIRCLE_BRANCH", ""), environ.get("CIRCLE_SHA1", "")
         )
-        f = open("%s" % (sys_env), "w")
+        f = open(sys_env, "w")
         f.write(data_worker)
         f.close()
 

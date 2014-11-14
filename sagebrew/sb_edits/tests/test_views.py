7d0414eaca470ef65fc95b4f87b81c517ba81111
tylerwiersing <tyler.wiersing@gmail.com>
2014-11-13 19:29:09 -0500

    began work on coverage

--- sagebrew/sb_edits/tests/test_views.py
+++ sagebrew/sb_edits/tests/test_views.py
@@ -1,1 +1,26 @@
-__author__ = 'tylerwiersing'
+from uuid import uuid1
+from base64 import b64encode
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.conf import settings
+
+from plebs.neo_models import Pleb
+from api.utils import test_wait_util
+from sb_registration.utils import create_user_util
+
+from sb_edits.views import edit_question_title_view, edit_object_view
+
+
+class TestEditQuestionView(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.email = "success@simulator.amazonses.com"
+        res = create_user_util("test", "test", self.email, "testpassword")
+        self.assertNotEqual(res, False)
+        test_wait_util(res)
+        self.pleb = Pleb.nodes.get(email=self.email)
+        self.user = User.objects.get(email=self.email)
+
+    def test_edit_question_view_success(self):
+        pass

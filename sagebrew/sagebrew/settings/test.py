8f8ae7c4fe39aa53f5f388797f4b5b14facccb8b
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-17 11:57:14 -0500

    Adding back in a few flags for nose tests

--- sagebrew/sagebrew/settings/test.py
+++ sagebrew/sagebrew/settings/test.py
@@ -30,6 +30,8 @@ CACHES = {
 INSTALLED_APPS += ('django_nose', )
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=sagebrew',
     '--stop',
 ]
 

b88c5119c53cd8986a7c4b73c0d5d2bd969cf67d
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-17 11:48:59 -0500

    Trying to just do coverage with coverage

--- sagebrew/sagebrew/settings/test.py
+++ sagebrew/sagebrew/settings/test.py
@@ -30,12 +30,7 @@ CACHES = {
 INSTALLED_APPS += ('django_nose', )
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=sagebrew',
-    '--cover-html',
     '--stop',
-    '--config=nose.cfg',
-    '--where=%s' % (PROJECT_DIR),
 ]
 
 

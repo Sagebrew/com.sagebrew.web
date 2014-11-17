91e9315f572ca8a512629a3758728303d724f3dc
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-17 17:49:52 -0500

    Fixing exception logger

--- sagebrew/sb_base/utils.py
+++ sagebrew/sb_base/utils.py
@@ -8,6 +8,6 @@ def defensive_exception(function_name, exception, return_value):
     logger.critical(dumps({'function': function_name,
                            'exception': 'Unhandled Exception',
                            'exception_dict': exception.__dict__}))
-    logger.exception()
+    logger.exception("defensive exception handler: ")
 
     return return_value

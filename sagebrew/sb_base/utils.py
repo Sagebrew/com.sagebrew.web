550d6094a005c0292948032827c001d1347e2c79
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-17 18:19:17 -0500

    Added documentation for defensive_exception

--- sagebrew/sb_base/utils.py
+++ sagebrew/sb_base/utils.py
@@ -5,6 +5,18 @@ logger = logging.getLogger('loggly_logs')
 
 
 def defensive_exception(function_name, exception, return_value, message=None):
+    """
+    defensive_exception is a utility that logs off an exception and then returns
+    whatever the calling function wanted returned in the scenario. This function
+    should be used for handling any unhandled exceptions and for any defensive
+    code. This way we can test the handling of the exceptions.
+
+    :param function_name: str
+    :param exception: exception object
+    :param return_value: duck typed
+    :param message: str or dict
+    :return: return_value
+    """
     error_dict = {'function': function_name,
                   'exception': 'Unhandled Exception',
                   'exception_dict': exception.__dict__}

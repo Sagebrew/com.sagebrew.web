6bea12c13e295ddc8d18a7ba11ab893f3f4ed9c4
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-19 00:21:51 -0500

    Adding template_name clarification to template view calls

--- sagebrew/sb_registration/urls.py
+++ sagebrew/sb_registration/urls.py
@@ -12,13 +12,15 @@ urlpatterns = patterns(
     'sb_registration.views',
     url(r'^signup/$', signup_view_api, name="signup_api"),
     url(r'^signup/confirm/$', login_required(
-        TemplateView.as_view('verify_email.html')), name="confirm_view"),
+        TemplateView.as_view(template_name='verify_email.html')),
+        name="confirm_view"),
 
     url(r'^login/api/$', login_view_api, name="login_api"),
     url(r'^profile_information/$', profile_information, name="profile_info"),
     url(r'^interests/$', interests, name="interests"),
     url(r'^profile_picture/$', profile_picture, name="profile_picture"),
-    url(r'^age_restriction/$', TemplateView.as_view('age_restriction_13.html'),
+    url(r'^age_restriction/$', TemplateView.as_view(
+        template_name='age_restriction_13.html'),
         name="age_restriction_13"),
     url(r'^email_confirmation/resend/$', resend_email_verification,
         name="resend_verification"),

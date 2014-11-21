ed74959249f111a83f2a846dbd0747610514068e
tylerwiersing <tyler.wiersing@gmail.com>
2014-11-21 14:36:32 -0500

    merge with devon-dev

--- sagebrew/sb_base/decorators.py
+++ sagebrew/sb_base/decorators.py
@@ -1,24 +1,31 @@
 from .utils import defensive_exception
 
 
-def apply_defense(function_with_defense, return_value=None,
-                  return_statement=True, message=None):
+class apply_defense(object):
     '''
     '''
-    def defensive_wrapper(*args, **kwargs):
-        '''
-        '''
-        try:
-            return function_with_defense(*args, **kwargs)
-        except Exception as e:
-            if return_value is None:
-                return_value_equals = e
-            else:
-                return_value_equals = return_value
-            if(return_statement is True):
-                return defensive_exception(function_with_defense.__name__, e,
-                                           return_value_equals, message)
-            else:
-                raise defensive_exception(function_with_defense.__name__, e,
-                                          return_value_equals, message)
-    return defensive_wrapper
+    def __init__(self, return_value=None, return_statement=True, message=None):
+        self.return_value = return_value
+        self.return_statement = return_statement
+        self.message = message
+
+    def __call__(self, function_with_defense):
+        def defensive_wrapper(*args, **kwargs):
+            '''
+            '''
+            try:
+                return function_with_defense(*args, **kwargs)
+            except Exception as e:
+                if self.return_value is None:
+                    return_value_equals = e
+                else:
+                    return_value_equals = self.return_value
+                if self.return_statement is True:
+                    return defensive_exception(function_with_defense.__name__,
+                                               e, return_value_equals,
+                                               self.message)
+                else:
+                    raise defensive_exception(function_with_defense.__name__,
+                                              e, return_value_equals,
+                                              self.message)
+        return defensive_wrapper

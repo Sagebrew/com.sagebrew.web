9236741f4b2d23e370e49b54599245d166bd1d78
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-22 22:24:18 -0500

    Trying out CypherException and ESException creation by changing domains

--- sagebrew/plebs/tests/test_utils.py
+++ sagebrew/plebs/tests/test_utils.py
@@ -1,4 +1,5 @@
 import shortuuid
+from os import environ
 from django.test import TestCase
 from django.contrib.auth.models import User
 
@@ -24,3 +25,15 @@ class TestPrepareUserSearchHTML(TestCase):
 
         self.assertIn('<div style="font-weight: bold">Reputation: 0 | '
                       'Mutual Friends: 0</div>', res)
+
+    def test_prepare_user_pleb_does_not_exist(self):
+        res = prepare_user_search_html("fake_email@fakegoogle.com")
+        self.assertFalse(res)
+
+    def test_cypher_exception(self):
+        temp_neo = environ['NEO4J_REST_URL']
+        environ['NEO4J_REST_URL'] = "http://sagebrew.com:7474/db/data/"
+        res = prepare_user_search_html(self.user.email)
+        environ["NEO4J_REST_URL"] = temp_neo
+        
+        self.assertIsNone(res)

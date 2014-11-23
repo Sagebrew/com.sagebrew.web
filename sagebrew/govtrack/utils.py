761891f875b18ca6b0055f06e45407d2cba2c36f
Devon Bleibtrey <bleib1dj@gmail.com>
2014-11-23 09:43:39 -0500

    Cleaning up some exception handling in gov track but still need catches for failed access to site

--- sagebrew/govtrack/utils.py
+++ sagebrew/govtrack/utils.py
@@ -1,50 +1,48 @@
-from json import dumps
-from logging import getLogger
 from datetime import datetime
-
+from neomodel import DoesNotExist, CypherException
 from govtrack.neo_models import (GTPerson, GTRole)
-from sb_base.utils import defensive_exception
 
-logger = getLogger('loggly_logs')
 
 def create_gt_role(rep):
     try:
+        my_role = GTRole.nodes.get(role_id=rep["id"])
+    except(GTRole.DoesNotExist, DoesNotExist):
+        my_person = create_gt_person(rep['person'])
+        rep.pop("person", None)
+        rep["role_id"] = rep["id"]
+        rep.pop("id", None)
+        rep["enddate"] = datetime.strptime(rep["enddate"],
+                                           '%Y-%m-%d')
+        rep["startdate"] = datetime.strptime(rep["startdate"],
+                                             '%Y-%m-%d')
+        temp_cong_num = rep.pop("congress_numbers", None)
+        my_role = GTRole(**rep)
         try:
-            my_role = GTRole.nodes.get(role_id=rep["id"])
-        except GTRole.DoesNotExist:
-            my_person = create_gt_person(rep['person'])
-            rep.pop("person", None)
-            rep["role_id"] = rep["id"]
-            rep.pop("id", None)
-            rep["enddate"] = datetime.strptime(rep["enddate"],
-                                               '%Y-%m-%d')
-            rep["startdate"] = datetime.strptime(rep["startdate"],
-                                                 '%Y-%m-%d')
-            temp_cong_num = rep.pop("congress_numbers", None)
-            my_role = GTRole(**rep)
             my_role.save()
             rep['congress_numbers'] = temp_cong_num
             my_person.role.connect(my_role)
             my_role.person.connect(my_person)
-
-        return my_role
-    except Exception as e:
-        return defensive_exception(create_gt_role.__name__, e,
-                                   False)
+        except(CypherException, IOError) as e:
+            return e
+    except(CypherException, IOError) as e:
+        return e
+    return my_role
 
 
 def create_gt_person(gt_person):
     try:
+        my_person = GTPerson.nodes.get(gt_id=gt_person["id"])
+    except(GTPerson.DoesNotExist, DoesNotExist):
+        gt_person["birthday"] = datetime.strptime(gt_person["birthday"],
+                                                  '%Y-%m-%d')
+        gt_person["gt_id"] = gt_person["id"]
+        gt_person.pop("id", None)
+        my_person = GTPerson(**gt_person)
         try:
-            my_person = GTPerson.nodes.get(gt_id=gt_person["id"])
-        except GTPerson.DoesNotExist:
-            gt_person["birthday"] = datetime.strptime(gt_person["birthday"],
-                                                      '%Y-%m-%d')
-            gt_person["gt_id"] = gt_person["id"]
-            gt_person.pop("id", None)
-            my_person = GTPerson(**gt_person)
             my_person.save()
+        except(CypherException, IOError) as e:
+            return e
+    except(CypherException, IOError) as e:
+        return e
 
-        return my_person
-    except Exception as e:
-        return defensive_exception(create_gt_person.__name__, e, False)
+    return my_person

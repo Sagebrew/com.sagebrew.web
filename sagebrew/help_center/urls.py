6bde70e4308a1efc5ab08cbcfd057d8b0351a5fd
Devon Bleibtrey <bleib1dj@gmail.com>
2014-12-15 20:42:37 -0500

    Updated some of the help articles, started some more organization of them, and removed flat-ui non-pro from the bower req file

--- sagebrew/help_center/urls.py
+++ sagebrew/help_center/urls.py
@@ -4,9 +4,15 @@ from django.views.generic import TemplateView
 urlpatterns = patterns(
     'help_center.views',
     url(r'^good_question/$', TemplateView.as_view(
-        template_name="good_question.html"),
+        template_name="asking/good_question.html"),
         kwargs={"title": "How do I create a good question?",
                 "description": "A frequently asked question regarding what is"
                                "expected from community members when they are"
                                "asking a question."}, name="good_question"),
+    url(r'^starting_a_private_conversation/$', TemplateView.as_view(
+        template_name="conversations/starting_a_private_conversation.html"),
+        kwargs={"title": "How do I create a good question?",
+                "description": "Explanation on how to start a private"
+                               " conversation between friends."},
+        name="starting_private_conversation"),
 )

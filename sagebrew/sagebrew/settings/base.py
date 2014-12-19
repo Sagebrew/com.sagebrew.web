4f42abc81f521e83f1f9ee6b2a783469f3b23910
Jordan Welton <jordan.welton@gmail.com>
2014-12-19 12:22:07 -0500

    cleaning up profile page elements

--- sagebrew/sagebrew/settings/base.py
+++ sagebrew/sagebrew/settings/base.py
@@ -75,7 +75,8 @@ STATICFILES_DIRS = (
     '%s/sb_tag/static/' % PROJECT_DIR,
     '%s/sb_flags/static/' % PROJECT_DIR,
     '%s/sb_votes/static/' % PROJECT_DIR,
-    '%s/sb_edits/static/' % PROJECT_DIR
+    '%s/sb_edits/static/' % PROJECT_DIR,
+    '%s/sb_wall/static/' % PROJECT_DIR
 )
 
 # List of finder classes that know how to find static files in

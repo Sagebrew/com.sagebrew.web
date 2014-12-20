e4b9486995a4145d3f2e9c69007f11c2a63b4f2b
tylerwiersing <tyler.wiersing@gmail.com>
2014-12-19 19:17:16 -0500

    created rep view and urls

--- sagebrew/sb_reps/views.py
+++ sagebrew/sb_reps/views.py
@@ -1,1 +1,19 @@
-__author__ = 'tylerwiersing'
+import pytz
+import logging
+from datetime import datetime
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from sb_registration.utils import (verify_completed_registration)
+
+@login_required()
+@user_passes_test(verify_completed_registration,
+                  login_url='/registration/profile_information')
+def representative_page(request, rep_id=""):
+    pass

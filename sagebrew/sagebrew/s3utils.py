39f8e4a4c3c270a3509eb8c74455356cc6a24739
Devon Bleibtrey <bleib1dj@gmail.com>
2014-12-24 16:45:11 -0500

    Updating static location for s3

--- sagebrew/sagebrew/s3utils.py
+++ sagebrew/sagebrew/s3utils.py
@@ -2,7 +2,6 @@ from storages.backends.s3boto import S3BotoStorage
 from django.core.files.storage import get_storage_class
 
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
 
 class CachedS3BotoStorage(S3BotoStorage):

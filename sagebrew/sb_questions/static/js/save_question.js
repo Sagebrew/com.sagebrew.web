24441860d584d238c09e85d30214ea82d1e8ec28
tylerwiersing <tyler.wiersing@gmail.com>
2015-02-01 16:43:20 -0500

    working on markdown

--- sagebrew/sb_questions/static/js/save_question.js
+++ sagebrew/sb_questions/static/js/save_question.js
@@ -12,7 +12,7 @@ $( document ).ready(function() {
 			url: "/questions/submit_question_api/",
 			data: JSON.stringify({
                'question_title': $('textarea#question_title_id').val(),
-			   'content': $('textarea#question_content_id').val(),
+			   'content': $('textarea#wmd-input-0').val(),
                'current_pleb':$(this).data('current_pleb'),
                'tags': $('#sb_tag_box').val()
 			}),
@@ -25,22 +25,3 @@ $( document ).ready(function() {
 	});
 });
 
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-function sameOrigin(url) {
-    // test that a given url is a same-origin URL
-    // url could be relative or scheme relative or absolute
-    var host = document.location.host; // host + port
-    var protocol = document.location.protocol;
-    var sr_origin = '//' + host;
-    var origin = protocol + sr_origin;
-    // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-        // or any other URL that isn't scheme relative or absolute i.e relative.
-        !(/^(\/\/|http:|https:).*/.test(url));
-}

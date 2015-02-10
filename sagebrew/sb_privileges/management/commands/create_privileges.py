b7fad1ff44c74acdcc577aa7e3f799efa2d1f457
tylerwiersing <tyler.wiersing@gmail.com>
2015-02-10 17:00:24 -0500

    management command to create actions

--- sagebrew/sb_privileges/management/commands/create_privileges.py
+++ sagebrew/sb_privileges/management/commands/create_privileges.py
@@ -11,7 +11,17 @@ class Command(BaseCommand):
     help = 'Creates privilege, requirement, action and restrictions.'
 
     def create_privileges(self):
-        pass
+        with open('%s/sb_privileges/management/commands'
+                  '/privilege_nodes.json' % settings.PROJECT_DIR,
+                  'r') as data_file:
+            data = loads(data_file.read())
+            for privilege in data['privileges']:
+                privilege = SBPrivilege(**privilege).save()
+            for action in data['actions']:
+                action = SBAction(**action).save()
+            for restriction in data['restrictions']:
+                restriction = SBRestriction(**restriction).save()
+            
 
     def handle(self, *args, **options):
         self.create_privileges()

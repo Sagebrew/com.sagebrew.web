17d10cb10162d149c92f44c8021f2b3952f70cec
tylerwiersing <tyler.wiersing@gmail.com>
2015-02-13 16:41:37 -0500

    management command to create badges and updated privilege creation command

--- sagebrew/sb_privileges/management/commands/create_privileges.py
+++ sagebrew/sb_privileges/management/commands/create_privileges.py
@@ -2,6 +2,8 @@ from json import loads
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
+from neomodel import CypherException
+
 from sb_privileges.neo_models import (SBPrivilege, SBRestriction, SBAction)
 from sb_requirements.neo_models import Requirement
 
@@ -16,12 +18,28 @@ class Command(BaseCommand):
                   'r') as data_file:
             data = loads(data_file.read())
             for privilege in data['privileges']:
-                privilege = SBPrivilege(**privilege).save()
+                requirements = privilege.pop('requirements', [])
+                try:
+                    privilege = SBPrivilege(**privilege).save()
+                except CypherException:
+                    pass
+                for requirement in requirements:
+                    try:
+                        req = Requirement(**requirement).save()
+                        privilege.requirements.connect(req)
+                    except CypherException:
+                        pass
             for action in data['actions']:
-                action = SBAction(**action).save()
+                try:
+                    action = SBAction(**action).save()
+                except CypherException:
+                    pass
             for restriction in data['restrictions']:
-                restriction = SBRestriction(**restriction).save()
-            
+                try:
+                    restriction = SBRestriction(**restriction).save()
+                except CypherException as e:
+                    pass
+
 
     def handle(self, *args, **options):
         self.create_privileges()

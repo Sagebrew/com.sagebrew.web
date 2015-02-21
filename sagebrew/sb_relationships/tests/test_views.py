607f73cd96b4edffce3f18498c66620e0c96ff4d
Devon Bleibtrey <bleib1dj@gmail.com>
2015-02-21 11:11:26 -0500

    Fixing additional tests and adding if to nav so that user can't use search or drop downs until after registration

--- sagebrew/sb_relationships/tests/test_views.py
+++ sagebrew/sb_relationships/tests/test_views.py
@@ -125,13 +125,13 @@ class TestGetFriendRequestsView(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.email = "success@simulator.amazonses.com"
-        res = create_user_util("test", "test", self.email, "testpassword")
+        res = create_user_util_test(self.email)
         self.assertNotEqual(res, False)
         wait_util(res)
         self.pleb = Pleb.nodes.get(email=self.email)
         self.user = User.objects.get(email=self.email)
         self.email2= "bounce@simulator.amazonses.com"
-        res = create_user_util("test", "test", self.email2, "testpassword")
+        res = create_user_util_test(self.email2)
         self.assertNotEqual(res, False)
         wait_util(res)
         self.pleb2 = Pleb.nodes.get(email=self.email2)

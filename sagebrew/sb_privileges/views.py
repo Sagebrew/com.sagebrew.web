ea08a0af552983aeaa4c254347d14ce935f427f6
tylerwiersing <tyler.wiersing@gmail.com>
2015-03-08 15:46:15 -0400

    WA-685 #time 30m

--- sagebrew/sb_privileges/views.py
+++ sagebrew/sb_privileges/views.py
@@ -105,9 +105,10 @@ class CreatePrivilege(APIView):
         print request.DATA
         action_forms = []
         requirement_forms = []
-        privilege_form = CreatePrivilegeForm(request.DATA or None)
-        if 'actions' in request.DATA:
-            if privilege_form.is_valid():
+        privilege_form = CreatePrivilegeForm(
+            {"privilege_name": request.DATA['privilege_name']})
+        if privilege_form.is_valid():
+            if 'actions' in request.DATA:
                 if type(request.DATA['actions']) == list:
                     for action in request.DATA['actions']:
                         action_form = SelectActionForm({"object_uuid":action})
@@ -125,27 +126,30 @@ class CreatePrivilege(APIView):
                     else:
                         return Response({"detail": "invalid action form",
                                          "errors": action_form.errors}, 400)
-        if 'requirements' in request.DATA:
-            if type(request.DATA['requirements']) == list:
-                for requirement in request.DATA['requirements']:
-                    requirement_form = SelectActionForm(
-                        {"object_uuid": requirement})
+            if 'requirements' in request.DATA:
+                if type(request.DATA['requirements']) == list:
+                    for requirement in request.DATA['requirements']:
+                        requirement_form = SelectActionForm(
+                            {"object_uuid": requirement})
+                        if requirement_form.is_valid():
+                            requirement_forms.append(
+                                requirement_form.cleaned_data)
+                            return Response(
+                                {"detail":"invalid requirement form",
+                                "errors": requirement_form.errors})
+                else:
+                    requirement_form = SelectRequirementForm(
+                        {"object_uuid": request.DATA['requirements']})
                     if requirement_form.is_valid():
                         requirement_forms.append(requirement_form.cleaned_data)
                         return Response({"detail":"invalid requirement form",
                                          "errors": requirement_form.errors})
-            else:
-                requirement_form = SelectRequirementForm(
-                    {"object_uuid": request.DATA['requirements']})
-                if requirement_form.is_valid():
-                    requirement_forms.append(requirement_form.cleaned_data)
-                    return Response({"detail":"invalid requirement form",
-                                     "errors": requirement_form.errors})
             task_data = {
                 "privilege_data": privilege_form.cleaned_data,
                 "actions": action_forms,
                 "requirements": requirement_forms
             }
+            print task_data
             return Response({"detail": "success"}, 200)
         else:
             return Response({"detail": "invalid privilege form",

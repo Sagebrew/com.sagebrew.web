bdcf5efa354317f5569f9e1a1b814935ec217663
tylerwiersing <tyler.wiersing@gmail.com>
2015-03-12 19:29:34 -0400

    WA-837 #time 20m #done

--- sagebrew/sb_registration/static/js/beta_signup.js
+++ sagebrew/sb_registration/static/js/beta_signup.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	$("button#beta_email_submit").click(function(event){
-        this.attr('disabled', 'disabled');
+        $("button#beta_email_submit").attr('disabled', 'disabled');
         event.preventDefault();
         $.ajaxSetup({
             beforeSend: function (xhr, settings) {

9a59b107b8aaf9cd92487bf401da9dedf3dd8183
Devon Bleibtrey <bleib1dj@gmail.com>
2015-03-13 17:05:07 -0400

    Reording some commands that should only be called if staging or master is being built

--- sagebrew/api/management/commands/dockerfilepopulation.py
+++ sagebrew/api/management/commands/dockerfilepopulation.py
@@ -11,8 +11,6 @@ class Command(BaseCommand):
         with open("%s/dockerfile_template" % settings.REPO_DIR,
                    "r") as dockerfile:
             circle_branch = os.environ.get("CIRCLE_BRANCH", None)
-            if "pull" in circle_branch:
-                circle_branch = "dev"
             data = dockerfile.read()
             if(circle_branch is not None):
                 data = data.replace("{{PROJECT_REPONAME}}",

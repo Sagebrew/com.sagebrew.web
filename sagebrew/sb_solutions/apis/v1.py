85f16d7cfd7fcdc7e1a7ed613f5e557f7db97b61
Devon Bleibtrey <bleib1dj@gmail.com>
2015-03-22 14:22:06 -0400

    Updated handling of versions to enable easier increments and fixed issue with DRF hyperlinked relations

--- sagebrew/sb_solutions/apis/v1.py
+++ sagebrew/sb_solutions/apis/v1.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 
 from rest_framework import routers
 
-from .endpoints import SolutionViewSet
+from sb_solutions.endpoints import SolutionViewSet
 
 router = routers.SimpleRouter()
 

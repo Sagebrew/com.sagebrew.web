8de78ca1487d071c205a76bae9567f0d0e5e07fd
tylerwiersing <tyler.wiersing@gmail.com>
2015-03-24 19:45:30 -0400

    Removing reference to trending in neo models

--- sagebrew/sb_search/neo_models.py
+++ sagebrew/sb_search/neo_models.py
@@ -47,8 +47,6 @@ class SearchQuery(StructuredNode):
     trending = BooleanProperty(default=False)
 
     #relationships
-    trending_weights = RelationshipTo('sb_trends.neo_models.TrendingWeight',
-                                      'TRENDING_WEIGHT')
     searched_by = Relationship('plebs.neo_models.Pleb', 'SEARCHED_BY')
     keywords = RelationshipTo(KeyWord, 'KEYWORDS', model=KeyWordRel)
     results = RelationshipTo(SearchResult, 'RESULT')

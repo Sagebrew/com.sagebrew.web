859fd58f25659598b7a6077fca04fe7274185d35
Devon Bleibtrey <bleib1dj@gmail.com>
2015-03-28 16:57:09 -0400

    Attempting to fix circular dependency issue

--- sagebrew/plebs/neo_models.py
+++ sagebrew/plebs/neo_models.py
@@ -10,11 +10,36 @@ from neomodel import (StructuredNode, StringProperty, IntegerProperty,
                       BooleanProperty, FloatProperty, ZeroOrOne,
                       CypherException, DoesNotExist)
 
-from sb_relationships.neo_models import (FriendRelationship,
-                                         UserWeightRelationship)
 from sb_base.neo_models import RelationshipWeight
 from sb_search.neo_models import SearchCount
 from sb_tag.neo_models import SBTag
+
+
+class FriendRelationship(StructuredRel):
+    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    friend_type = StringProperty(default="friends")
+    currently_friends = BooleanProperty(default=True)
+    time_unfriended = DateTimeProperty(default=None)
+    who_unfriended = StringProperty()
+    # who_unfriended = RelationshipTo("Pleb", "")
+
+
+class UserWeightRelationship(StructuredRel):
+    interaction = StringProperty(default='seen')
+    page_view_count = IntegerProperty(default=0)
+    weight = IntegerProperty(default=settings.USER_RELATIONSHIP_BASE['seen'])
+
+
+class FriendRequest(StructuredNode):
+    object_uuid = StringProperty(unique_index=True)
+    seen = BooleanProperty(default=False)
+    time_sent = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    time_seen = DateTimeProperty(default=None)
+    response = StringProperty(default=None)
+
+    # relationships
+    request_from = RelationshipTo('plebs.neo_models.Pleb', 'REQUEST_FROM')
+    request_to = RelationshipTo('plebs.neo_models.Pleb', 'REQUEST_TO')
 
 
 class TagRelationship(StructuredRel):
@@ -44,7 +69,6 @@ class OfficialRelationship(StructuredRel):
     active = BooleanProperty(default=False)
     start_date = DateTimeProperty()
     end_date = DateTimeProperty()
-
 
 
 class Pleb(StructuredNode):
--- sagebrew/sb_relationships/neo_models.py
+++ sagebrew/sb_relationships/neo_models.py
@@ -7,28 +7,3 @@ from neomodel import (StructuredNode, StringProperty, DateTimeProperty,
                       BooleanProperty, IntegerProperty)
 
 
-class FriendRelationship(StructuredRel):
-    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
-    friend_type = StringProperty(default="friends")
-    currently_friends = BooleanProperty(default=True)
-    time_unfriended = DateTimeProperty(default=None)
-    who_unfriended = StringProperty()
-    # who_unfriended = RelationshipTo("Pleb", "")
-
-
-class UserWeightRelationship(StructuredRel):
-    interaction = StringProperty(default='seen')
-    page_view_count = IntegerProperty(default=0)
-    weight = IntegerProperty(default=settings.USER_RELATIONSHIP_BASE['seen'])
-
-
-class FriendRequest(StructuredNode):
-    object_uuid = StringProperty(unique_index=True)
-    seen = BooleanProperty(default=False)
-    time_sent = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
-    time_seen = DateTimeProperty(default=None)
-    response = StringProperty(default=None)
-
-    # relationships
-    request_from = RelationshipTo('plebs.neo_models.Pleb', 'REQUEST_FROM')
-    request_to = RelationshipTo('plebs.neo_models.Pleb', 'REQUEST_TO')

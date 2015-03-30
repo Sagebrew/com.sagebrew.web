957b2e7db19a78bd1b28f94e3d60fb441d45aeba
Devon Bleibtrey <bleib1dj@gmail.com>
2015-03-30 18:49:25 -0400

    Removing tests for get_post function that was removed

--- sagebrew/sb_posts/tests/test_views.py
+++ sagebrew/sb_posts/tests/test_views.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 from api.utils import wait_util
 from plebs.neo_models import Pleb
-from sb_posts.views import (save_post_view, get_user_posts)
+from sb_posts.views import (save_post_view)
 from sb_registration.utils import create_user_util_test
 from sb_posts.neo_models import SBPost
 
@@ -92,44 +92,3 @@ class SavePostViewTests(TestCase):
         response = save_post_view(request)
 
         self.assertEqual(response.status_code, 400)
-
-class TestGetUserPosts(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.email = "success@simulator.amazonses.com"
-        res = create_user_util_test(self.email)
-        self.assertNotEqual(res, False)
-        wait_util(res)
-        self.pleb = Pleb.nodes.get(email=self.email)
-        self.user = User.objects.get(email=self.email)
-
-    def test_get_user_posts(self):
-        for item in range(0, 3):
-            post = SBPost(
-                content='test', object_uuid=str(uuid1()),
-                created=(datetime.now(pytz.utc) + timedelta(hours=10))).save()
-            rel = post.owned_by.connect(self.pleb)
-            rel.save()
-            self.pleb.wall.all()[0].post.connect(post)
-
-        data = {'current_user': self.pleb.username,
-                'range_end': 1,
-                'range_start': 0}
-        request = self.factory.post('/posts/query_posts/', data=data,
-                                    format='json')
-        request.user = self.user
-        res = get_user_posts(request)
-
-        self.assertEqual(res.status_code, 200)
-
-    def test_get_user_posts_invalid_form(self):
-        data = {'curreasdfnt_user': self.pleb.email,
-                'email': self.pleb.email,
-                'range_end': 5,
-                'range_start': 0}
-        request = self.factory.post('/posts/query_posts/', data=data,
-                                    format='json')
-        request.user = self.user
-        res = get_user_posts(request)
-
-        self.assertEqual(res.status_code, 400)

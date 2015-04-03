aace497a713a77ec630959500019de14ee321c53
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-03 17:00:19 -0400

    working on search filtering

--- sagebrew/sb_search/urls.py
+++ sagebrew/sb_search/urls.py
@@ -5,7 +5,7 @@ from .views import search_result_view, search_view, search_result_api
 urlpatterns = patterns(
     'sb_search.views',
     url(r'^$', search_view, name="search"),
-    url(r'^q=(?P<query_param>.+?)&page=(?P<page>[0-9]{1,100})$',
+    url(r'^q=(?P<query_param>.+?)&page=(?P<page>[0-9]{1,100})&filter=(?P<filter>[0-9]{1,100})$',
         search_result_view, name="search_results"),
     url(r'^api/q=(?P<query_param>.+?)&page=(?P<page>[0-9]{1,100})$',
         search_result_api, name="search_result_api"),

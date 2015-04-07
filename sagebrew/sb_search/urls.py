3bf6302fc3e22d82c349be278be8ab1297d684fa
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-07 15:29:15 -0400

    some formatting

--- sagebrew/sb_search/urls.py
+++ sagebrew/sb_search/urls.py
@@ -4,8 +4,6 @@ from .views import search_result_view, search_result_api
 
 urlpatterns = patterns(
     'sb_search.views',
-    url(r'^api/$',
-        search_result_api, name="search_result_api"),
-    url(r'^$',
-        search_result_view, name="search_results"),
+    url(r'^api/$', search_result_api, name="search_result_api"),
+    url(r'^$', search_result_view, name="search_results"),
 )

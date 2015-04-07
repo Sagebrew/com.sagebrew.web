f696a780fb3a9fb643996144d811c6210c9f0985
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-07 14:47:24 -0400

    working on new tests for search

--- sagebrew/sb_public_official/tests/test_utils.py
+++ sagebrew/sb_public_official/tests/test_utils.py
@@ -1,9 +1,15 @@
+import pytz
+from uuid import uuid1
+from datetime import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
+
+from neomodel import CypherException
 
 from api.utils import wait_util
 from plebs.neo_models import Pleb
 from sb_registration.utils import create_user_util_test
+from sb_public_official.neo_models import BaseOfficial
 from sb_public_official.utils import prepare_official_search_html
 
 
@@ -16,3 +22,24 @@ class TestPrepareSearchHTML(TestCase):
         wait_util(res)
         self.pleb = Pleb.nodes.get(email=self.email)
         self.user = User.objects.get(email=self.email)
+
+    def test_prepare_search_html(self):
+        try:
+            test_official = BaseOfficial(
+                object_uuid = str(uuid1()),
+                full_name = "Test Rep",
+                first_name = "Test",
+                last_name = "Rep",
+                start_date = datetime.now(pytz.utc),
+                end_date = datetime.now(pytz.utc),
+                state = "MI",
+                title = "House Rep",
+                district = 11,
+                current = True
+            )
+            test_official.save()
+        except (CypherException, IOError):
+            pass
+
+        res = prepare_official_search_html(test_official.object_uuid)
+        self.assertIsInstance(res, str)

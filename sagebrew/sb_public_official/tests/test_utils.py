a14119711c1c274808b7704ba144aa5acb824157
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-07 14:40:34 -0400

    updated the saga search html to include a link to the reps page

---
+++ sagebrew/sb_public_official/tests/test_utils.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from api.utils import wait_util
+from plebs.neo_models import Pleb
+from sb_registration.utils import create_user_util_test
+from sb_public_official.utils import prepare_official_search_html
+
+
+
+class TestPrepareSearchHTML(TestCase):
+    def setUp(self):
+        self.email = "success@simulator.amazonses.com"
+        res = create_user_util_test(self.email)
+        self.assertNotEqual(res, False)
+        wait_util(res)
+        self.pleb = Pleb.nodes.get(email=self.email)
+        self.user = User.objects.get(email=self.email)

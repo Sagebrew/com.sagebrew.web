2d28df795f2b2ef483676ad796312095cb4677a8
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-11 13:33:26 -0400

    More tests removed

--- sagebrew/sb_comments/tests/test_tasks.py
+++ sagebrew/sb_comments/tests/test_tasks.py
@@ -5,52 +5,12 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from api.utils import wait_util
-from sb_comments.tasks import (save_comment_on_object,
-                               create_comment_relations)
+from sb_comments.tasks import (create_comment_relations)
 from plebs.neo_models import Pleb
 from sb_registration.utils import create_user_util_test
 from sb_questions.neo_models import Question
 from sb_comments.neo_models import Comment
 
-
-class TestSaveCommentTask(TestCase):
-    def setUp(self):
-        self.email = "success@simulator.amazonses.com"
-        res = create_user_util_test(self.email)
-        self.assertNotEqual(res, False)
-        wait_util(res)
-        self.pleb = Pleb.nodes.get(email=self.email)
-        self.user = User.objects.get(email=self.email)
-        settings.CELERY_ALWAYS_EAGER = True
-
-    def tearDown(self):
-        settings.CELERY_ALWAYS_EAGER = False
-
-    def test_save_comment_on_object_task_success(self):
-        question = Question(object_uuid=str(uuid1())).save()
-        task_param = {'content': 'test comment',
-                      'username': self.pleb.username,
-                      'object_uuid': question.object_uuid,
-                      'object_type': 'sb_questions.neo_models.Question',
-                      'comment_uuid': str(uuid1())}
-        response = save_comment_on_object.apply_async(kwargs=task_param)
-        while not response.ready():
-            time.sleep(1)
-
-        self.assertTrue(response.result)
-
-    def test_save_comment_on_object_task_get_object_fail(self):
-        question = Question(object_uuid=str(uuid1())).save()
-        task_param = {'content': 'test comment',
-                      'username': self.pleb.username,
-                      'object_uuid': question.object_uuid,
-                      'object_type': 'Question',
-                      'comment_uuid': str(uuid1())}
-        response = save_comment_on_object.apply_async(kwargs=task_param)
-        while not response.ready():
-            time.sleep(1)
-
-        self.assertFalse(response.result)
 
 class TestCreateCommentRelationsTask(TestCase):
     def setUp(self):

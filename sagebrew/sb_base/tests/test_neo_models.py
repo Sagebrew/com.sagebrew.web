81839f1337c29e188e800f4f23b44d5a6d9d9a98
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-13 21:49:22 -0400

    Removing unnused tests

--- sagebrew/sb_base/tests/test_neo_models.py
+++ sagebrew/sb_base/tests/test_neo_models.py
@@ -6,7 +6,6 @@ from api.utils import wait_util
 from plebs.neo_models import Pleb
 from sb_registration.utils import create_user_util_test
 from sb_posts.neo_models import Post
-from sb_base.neo_models import SBContent
 
 
 class TestVotableContentNeoModel(TestCase):
@@ -49,28 +48,5 @@ class TestVotableContentNeoModel(TestCase):
         rel.save()
 
         res = self.post.remove_vote(rel)
-
-        self.assertIsInstance(res, Post)
-
-
-class TestSBContentNeoModel(TestCase):
-    def setUp(self):
-        self.email = "success@simulator.amazonses.com"
-        res = create_user_util_test(self.email)
-        self.assertNotEqual(res, False)
-        wait_util(res)
-        self.pleb = Pleb.nodes.get(email=self.email)
-        self.user = User.objects.get(email=self.email)
-        self.content = SBContent(content='test', object_uuid=str(uuid1())).save()
-        self.post = Post(content='fake content', object_uuid=str(uuid1())).save()
-
-    def test_flag_content_unavailable_flag(self):
-        res = self.post.flag_content('changed', self.pleb)
-
-        self.assertFalse(res)
-
-    def test_flag_content_user_already_flagged(self):
-        self.post.flagged_by.connect(self.pleb)
-        res = self.post.flag_content('spam', self.pleb)
 
         self.assertIsInstance(res, Post)

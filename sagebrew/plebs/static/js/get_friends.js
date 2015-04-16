15bddef21ea9f0b4e2d129e8e79324fc5db32cda
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-16 15:08:40 -0400

    removed some prints, added scroll functionality into friends page

--- sagebrew/plebs/static/js/get_friends.js
+++ sagebrew/plebs/static/js/get_friends.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var username = $("#user_info").data("page_user_username");
+    var scrolled = false;
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
             ajax_security(xhr, settings)
@@ -12,11 +13,44 @@ $(document).ready(function(){
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data) {
-            $("#friend_wrapper").append(data['results'])
+            $("#friend_wrapper").append(data['results']);
+            $("#next_url").data('url', data['next']);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             if(XMLHttpRequest.status === 500){
                 $("#server_error").show();
+            }
+        }
+    });
+    $(window).scroll(function() {
+        if(scrolled == false) {
+            if ($(window).scrollTop() + $(window).height() > ($(document).height() - $(document).height()*.5)) {
+                scrolled = true;
+                var next = $("#next_url").data('url');
+                if (next !== null)
+                {
+                    $.ajaxSetup({beforeSend: function (xhr, settings) {
+                        ajax_security(xhr, settings)
+                    }
+                    });
+                    $.ajax({
+                        xhrFields: {withCredentials: true},
+                        type: "GET",
+                        url: next,
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            scrolled = false;
+                            $("#friend_wrapper").append(data['results']);
+                            $("#next_url").data('url', data['next']);
+                        },
+                        error: function(XMLHttpRequest, textStatus, errorThrown) {
+                            if(XMLHttpRequest.status === 500){
+                                $("#server_error").show();
+                            }
+                        }
+                    });
+                }
             }
         }
     });

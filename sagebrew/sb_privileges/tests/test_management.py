8a6854c3011807e3560733096b0147e3f754ccc6
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-17 11:59:32 -0400

    More test fixes

--- sagebrew/sb_privileges/tests/test_management.py
+++ sagebrew/sb_privileges/tests/test_management.py
@@ -27,7 +27,7 @@ class TestCreatePrivileges(TestCase):
                         name=requirement["name"])
                     self.assertIsNotNone(requirement_obj)
                 for action in actions:
-                    action_obj = SBAction.nodes.get(permission=action["permission"])
+                    action_obj = SBAction.nodes.get(resource=action["resource"])
                     self.assertIsNotNone(action_obj)
                 for restriction in data['restrictions']:
                     restrict_obj = Restriction.nodes.get(

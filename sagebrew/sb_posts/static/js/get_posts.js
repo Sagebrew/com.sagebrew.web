47f90312c3fb9494be29ce6245d0469ed258d7a1
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-17 15:37:42 -0400

    removing some scroll loading functionality

--- sagebrew/sb_posts/static/js/get_posts.js
+++ sagebrew/sb_posts/static/js/get_posts.js
@@ -1,16 +1,3 @@
 $( document ).ready(function() {
-    var scrolled = false;
-    var data = loadPosts("/v1/profiles/" + $('#user_info').data('page_user_username') + "/wall/render/?page_size="+ 2 + "&expand=true");
-    $(window).scroll(function() {
-        if(scrolled == false) {
-            if ($(window).scrollTop() + $(window).height() > ($(document).height() - $(document).height()*.5)) {
-                scrolled = true;
-                if (data['next'] !== null)
-                {
-                    data = loadPosts(data['next']);
-                    scrolled = false;
-                }
-            }
-        }
-    });
+    loadPosts("/v1/profiles/" + $('#user_info').data('page_user_username') + "/wall/render/?page_size="+ 2 + "&expand=true");
 });

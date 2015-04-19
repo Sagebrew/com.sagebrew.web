1ad99b1ad6faa6c9bdab3a4fb06f913f9f462801
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-19 15:17:57 -0400

    Removing dependency on waiting for async to complete to populate gt_roles

--- sagebrew/govtrack/management/commands/populate_rep_models.py
+++ sagebrew/govtrack/management/commands/populate_rep_models.py
@@ -1,4 +1,3 @@
-import time
 from django.core.management.base import BaseCommand
 
 from govtrack.tasks import populate_gt_role
@@ -10,19 +9,17 @@ class Command(BaseCommand):
 
     def populate_gt_reps(self, mi_only=False):
         if mi_only:
-            res = populate_gt_role.apply_async(
+            populate_gt_role.apply_async(
                 kwargs={
                     "requesturl": "https://www.govtrack.us/api/v2/role?"
                                   "state=MI&current=true"
                 })
         else:
-            res = populate_gt_role.apply_async(
+            populate_gt_role.apply_async(
                 kwargs={
                     "requesturl": "https://www.govtrack.us/api/v2/role?"
                                   "current=true&limit=600"
                 })
-        while not res.ready():
-            time.sleep(1)
         print "Created GT Objects"
 
     def handle(self, *args, **options):

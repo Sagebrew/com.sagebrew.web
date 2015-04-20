057601297c59897240d92ff0fef82ac3d180fa78
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-20 19:30:54 -0400

    WA-1178 #done

--- sagebrew/sb_search/static/js/submit_search.js
+++ sagebrew/sb_search/static/js/submit_search.js
@@ -3,11 +3,10 @@ $( document ).ready(function() {
         var search_param = ($('#sb_search_input').val());
         window.location.href = "/search/?q=" + search_param +"&page=1&filter=general";
 	});
+    $("#sb_search_input").keyup(function(e) {
+        if(e.which == 10 || e.which == 13) {
+            var search_param = ($('#sb_search_input').val());
+            window.location.href = "/search/?q=" + search_param + "&page=1&filter=general";
+        }
+    })
 });
-
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-

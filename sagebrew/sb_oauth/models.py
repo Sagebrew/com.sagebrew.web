32fc4d4e8d87dae85657388e9d2755baaa225704
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-23 20:15:04 -0400

    Moving token generation receiver to model file where we actually create a model

--- sagebrew/sb_oauth/models.py
+++ sagebrew/sb_oauth/models.py
@@ -1,6 +1,22 @@
+from logging import getLogger
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
+
+from rest_framework.authtoken.models import Token
 from oauth2_provider.models import AbstractApplication
+
+logger = getLogger('loggly_logs')
 
 
 class SBApplication(AbstractApplication):
     web_hook = models.URLField()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        logger.critical("creating token")
+        Token.objects.create(user=instance)

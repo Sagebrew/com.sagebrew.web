03ff609a6128d34369782a2694caefb158dfe994
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-24 20:03:23 -0400

    Fixing references to old django rest framework and neomodel versions

--- sagebrew/sb_questions/static/js/save_question.js
+++ sagebrew/sb_questions/static/js/save_question.js
@@ -1,38 +1,41 @@
-$( document ).ready(function() {
-	$(".submit_question-action").click(function(event){
-		event.preventDefault();
-		$.ajaxSetup({
-		    beforeSend: function (xhr, settings) {
-                ajax_security(xhr, settings)
+/*global $, jQuery, ajax_security*/
+$(document).ready(function () {
+    "use strict";
+    $(".submit_question-action").click(function (event) {
+        event.preventDefault();
+        $(".submit_question-action").attr("disabled", "disabled");
+        $.ajaxSetup({
+            beforeSend: function (xhr, settings) {
+                ajax_security(xhr, settings);
             }
-		});
+        });
         var tags = $('#sb_tag_box').val();
-        if(tags === ""){
-            tags = []
+        if (tags === "") {
+            tags = [];
         }
 
-	   	$.ajax({
-			xhrFields: {withCredentials: true},
-			type: "POST",
-			url: $(this).data('url'),
-			data: JSON.stringify({
-               'title': $('input#title_id').val(),
-			   'content': $('textarea#wmd-input-0').val(),
-               'tags': tags
-			}),
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
+        $.ajax({
+            xhrFields: {withCredentials: true},
+            type: "POST",
+            url: $(this).data('url'),
+            data: JSON.stringify({
+                'title': $('input#title_id').val(),
+                'content': $('textarea#wmd-input-0').val(),
+                'tags': tags
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
             success: function (data) {
-                window.location.href = data['url']
+                window.location.href = data.url;
             },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                if(XMLHttpRequest.status === 500){
+            error: function (XMLHttpRequest) {
+                if (XMLHttpRequest.status === 500) {
                     $("#server_error").show();
                 }
             }
-		});
-	});
-    $(".cancel_question-action").click(function(event){
-		window.location.href = "/conversations/";
-	});
+        });
+    });
+    $(".cancel_question-action").click(function () {
+        window.location.href = "/conversations/";
+    });
 });

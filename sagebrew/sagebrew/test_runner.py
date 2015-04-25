f5e9a8e45225731ee05aa0bb69078b5283bd436d
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-25 13:07:55 -0400

    Rearranging import

--- sagebrew/sagebrew/test_runner.py
+++ sagebrew/sagebrew/test_runner.py
@@ -6,10 +6,10 @@ from django.test.runner import DiscoverRunner
 
 class SBTestRunner(DiscoverRunner):
     def setup_databases(self, *args, **kwargs):
+        from neomodel import db
         call("/webapps/neo-test/neo4j-community-2.1.7/bin/neo4j start",
              shell=True)
         environ['NEO4J_REST_URL'] = "http://127.0.0.1:8484/db/data/"
-        from neomodel import db
         query = "start n=node(*) optional match (n)-[r]-() delete n,r"
         db.cypher_query(query)
         super(SBTestRunner, self).__init__(*args, **kwargs)

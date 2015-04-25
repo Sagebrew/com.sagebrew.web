dbd2c8953efd10f3a18a78e5a9d8b473e1296361
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-25 18:48:54 -0400

    added in basic validation of passed tagged_as parameter

--- sagebrew/sb_base/utils.py
+++ sagebrew/sb_base/utils.py
@@ -73,7 +73,7 @@ def get_ordering(sort_by):
 
 
 def get_tagged_as(tagged_as):
-    if tagged_as == '':
+    if tagged_as == '' or tagged_as not in settings.BASE_TAGS:
         return tagged_as
     return "-[:TAGGED_AS]-(t:Tag {name:'%s'}) " % (tagged_as)
 

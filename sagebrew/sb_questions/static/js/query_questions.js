9aa18474429024751bff4113884b27688f6812b3
tylerwiersing <tyler.wiersing@gmail.com>
2015-04-26 16:19:20 -0400

    finishing rebase

--- sagebrew/sb_questions/static/js/query_questions.js
+++ sagebrew/sb_questions/static/js/query_questions.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
         }
         $.ajaxSetup({
             beforeSend: function (xhr, settings) {
-                ajax_security(xhr, settings)
+                ajaxSecurity(xhr, settings)
             }
         });
         $.ajax({

ad4fc972c929de8be54c7ee46f56df99901bcdad
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-28 21:01:59 -0400

    Added caching for templates

--- sagebrew/sagebrew/settings/base.py
+++ sagebrew/sagebrew/settings/base.py
@@ -90,13 +90,6 @@ STATICFILES_FINDERS = (
 )
 
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    # 'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'opbeat.contrib.django.middleware.OpbeatAPMMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -152,6 +145,19 @@ TEMPLATE_DIRS = (
     '%s/sb_search/templates/' % PROJECT_DIR,
     '%s/sb_tags/templates/' % PROJECT_DIR,
 )
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': TEMPLATE_DIRS,
+    'OPTIONS': {
+        'loaders': [
+            ('django.template.loaders.cached.Loader', [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ]),
+        ],
+    },
+}]
 
 
 INSTALLED_APPS = (

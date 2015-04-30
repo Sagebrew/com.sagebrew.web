fd9be5c7a6bca87cc37e7d1e2ad441d1aa6ef53c
Devon Bleibtrey <bleib1dj@gmail.com>
2015-04-30 18:16:36 -0400

    Fixing flag modal issue

--- sagebrew/sb_flags/static/js/flag_modal.js
+++ sagebrew/sb_flags/static/js/flag_modal.js
@@ -4,26 +4,26 @@ $(document).ready(function () {
     $("textarea.flag_content_reason").pagedownBootstrap();
     $('#flagModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget),
-            flag_target = button.data('href'),
-            object_uuid = button.data('object_uuid'),
+            flagTarget = button.data('href'),
+            objectUUID = button.data('object_uuid'),
             modal = $(this);
-        modal.data('flag_target', flag_target);
-        modal.data('object_uuid', object_uuid);
-        modal.modal('handleUpdate');
+        modal.data('flag_target', flagTarget);
+        modal.data('object_uuid', objectUUID);
     });
     $('#flag_modal_submit').on('click', function (event) {
-        var flag_modal = $('#flagModal');
+        var flagModal = $('#flagModal');
         $("#flag_modal_submit").attr("disabled", "disabled");
         $.ajaxSetup(
             {
                 beforeSend: function (xhr, settings) {
-                    ajaxSecurity(xhr, settings)
+                    ajaxSecurity(xhr, settings);
                 }
-            });
+            }
+        );
         $.ajax({
             xhrFields: {withCredentials: true},
             type: "POST",
-            url: flag_modal.data('flag_target'),
+            url: flagModal.data('flag_target'),
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({
                 'content': $("textarea.flag_content_reason").val(),
@@ -31,14 +31,14 @@ $(document).ready(function () {
             }),
             dataType: "json",
             success: function (data) {
-                var object_uuid = flag_modal.data('object_uuid');
-                $("#flag_" + object_uuid).replaceWith('<i class="fa fa-flag sb_btn_icon sb_btn_icon_purple sb_span_icon"></i>');
+                var flagObjectUUID = flagModal.data('object_uuid');
+                $("#flag_" + flagObjectUUID).replaceWith('<i class="fa fa-flag sb_btn_icon sb_btn_icon_purple sb_span_icon"></i>');
                 $("#flag_modal_submit").removeAttr("disabled");
                 $('textarea.flag_content_reason').val("");
                 $('#wmd-preview-0').html("");
-                flag_modal.modal('hide');
+                flagModal.modal('hide');
             },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
+            error: function (XMLHttpRequest) {
                 $("#flag_modal_submit").removeAttr("disabled");
                 if (XMLHttpRequest.status === 500) {
                     $("#server_error").show();

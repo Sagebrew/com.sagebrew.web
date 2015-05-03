5944b3c74da07018d54b76c3780d1286c2f95266
tylerwiersing <tyler.wiersing@gmail.com>
2015-05-03 17:28:32 -0400

    initial commit for upload endpoint tests

---
+++ sagebrew/sb_uploads/tests/test_endpoints.py
@@ -0,0 +1,26 @@
+import shortuuid
+
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.core.cache import cache
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from sagebrew import errors
+from plebs.neo_models import Pleb, FriendRequest
+from sb_uploads.neo_models import UploadedObject, ModifiedObject
+from sb_registration.utils import create_user_util_test
+
+
+class UploadEndpointTests(APITestCase):
+    def setUp(self):
+        self.unit_under_test_name = 'pleb'
+        self.email = "success@simulator.amazonses.com"
+        create_user_util_test(self.email)
+        self.pleb = Pleb.nodes.get(email=self.email)
+        self.user = User.objects.get(email=self.email)
+        self.url = "http://testserver"
+
+    def test_unauthorized(self):
+        pass

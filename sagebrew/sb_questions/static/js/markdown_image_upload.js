7124daa6837f871e22237bb85d9c5959ba0814fb
tylerwiersing <tyler.wiersing@gmail.com>
2015-05-06 18:16:20 -0400

    updated markdown_image_upload

--- sagebrew/sb_questions/static/js/markdown_image_upload.js
+++ sagebrew/sb_questions/static/js/markdown_image_upload.js
@@ -1,9 +1,10 @@
+/*global $, jQuery, ajaxSecurity*/
 $(document).ready(function () {
+    "use strict";
     editor.hooks.set("insertImageDialog", function (callback) {
-        setTimeout(function ()
-        {
+        setTimeout(function () {
             $('#fileModal').modal();
-            $("#insert_image_post").click(function(e) {
+            $("#insert_image_post").click(function (e) {
                 e.preventDefault();
                 var image = $("#img-url").val();
                 callback(image);
@@ -11,6 +12,6 @@ $(document).ready(function () {
             });
         }, 0);
         return true; // tell the editor that we'll take care of getting the image url
-     });
+    });
     editor.run();
 });

c181e5b90d4529ea569bafdbd66a83fe3c304694
Devon Bleibtrey <bleib1dj@gmail.com>
2015-05-09 12:12:28 -0400

    Trying to wrap up ebextensions into zip to deploy

--- deployment/master.sh
+++ deployment/master.sh
@@ -6,21 +6,24 @@ docker push sagebrew/sb_worker:$SHA1
 docker push sagebrew/sys_util:$SHA1
 EB_BUCKET=sagebrew-$CIRCLE_BRANCH
 DOCKERRUN_FILE_WEB=$SHA1-master_web.aws.json
+WEB_ZIP=$SHA1-master_web.zip
 DOCKERRUN_FILE_WORKER=$SHA1-master_worker.aws.json
+WORKER_ZIP=$SHA1-master_worker.zip
 DOCKERRUN_WORKER_ENVIRONMENT=/home/ubuntu/com.sagebrew.web/$SHA1-master_worker.json
 DOCKERRUN_FILE_SYS=$SHA1-master_sys-util.aws.json
 
 sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_worker/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.worker_template > $DOCKERRUN_FILE_WORKER
 sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sb_web/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.web_template > $DOCKERRUN_FILE_WEB
 sed "s/<TAG>/$SHA1/;s/<PROJECT_NAME>/$PROJECT_NAME/;s/<BUCKET>/$CIRCLE_BRANCH/;s/<IMAGE>/sys_util/;" < ~/com.sagebrew.web/aws_templates/Dockerrun.aws.json.web_template > $DOCKERRUN_FILE_SYS
-
+zip $WEB_ZIP $DOCKERRUN_FILE_WEB /home/ubuntu/com.sagebrew.web/.ebextensions
+zip $WORKER_ZIP $DOCKERRUN_FILE_WEB /home/ubuntu/com.sagebrew.web/.ebextensions
 
 # Make sure to set AWS_DEFAULT_REGION to us-east-1 https://pypi.python.org/pypi/awscli
 # Also need to make sure to set permissions on dockercfg file so that all authenticated users
 # can access it
 # Make sure to add AWS creds to circle
-aws s3 cp $DOCKERRUN_FILE_WEB s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$DOCKERRUN_FILE_WEB
-aws s3 cp $DOCKERRUN_FILE_WORKER s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$DOCKERRUN_FILE_WORKER
+aws s3 cp $WEB_ZIP s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$WEB_ZIP
+aws s3 cp $WORKER_ZIP s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$WORKER_ZIP
 aws s3 cp $DOCKERRUN_FILE_SYS s3://$EB_BUCKET/$DOCKER_CONFIG_BUCKET/$DOCKERRUN_FILE_SYS
 
 ~/virtualenvs/venv-system/bin/python ~/com.sagebrew.web/deployment/deployment.py master

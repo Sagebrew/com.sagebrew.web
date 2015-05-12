fa13ce450669491d925702e1a9bf2b152308c261
Devon Bleibtrey <bleib1dj@gmail.com>
2015-05-12 12:16:49 -0400

    Adding expedite capabilities to post serializer

--- sagebrew/sb_posts/serializers.py
+++ sagebrew/sb_posts/serializers.py
@@ -42,10 +42,15 @@ class PostSerializerNeo(ContentSerializer):
         return instance
 
     def get_url(self, obj):
-        return obj.get_url(self.context.get('request', None))
+        request, _, _, _, expedite = gather_request_data(self.context)
+        if expedite == "true":
+            return None
+        return obj.get_url(request)
 
     def get_wall_owner_profile(self, obj):
-        request, expand, _, _, _ = gather_request_data(self.context)
+        request, expand, _, _, expedite = gather_request_data(self.context)
+        if expedite == "true":
+            return None
         if isinstance(obj, dict) is True:
             return obj
         wall_owner = obj.get_wall_owner_profile()

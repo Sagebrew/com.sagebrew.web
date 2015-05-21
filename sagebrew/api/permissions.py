b9eef194fd42c6797a3b3c0253a97f16a6d7b137
Devon Bleibtrey <bleib1dj@gmail.com>
2015-05-21 10:34:28 -0400

    Fixing property access issue

--- sagebrew/api/permissions.py
+++ sagebrew/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if (obj.username == request.user.username or
+        if (obj.owner_username == request.user.username or
                 request.user.is_staff):
             return True
         else:

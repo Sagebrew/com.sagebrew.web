67e48d9d843484eee3c135bb99e4c8eac2c5591a
tylerwiersing <tyler.wiersing@gmail.com>
2015-06-08 18:24:44 -0400

    refactored location data population management command to be able to run multiple times and not harm anything

--- sagebrew/sb_locations/management/commands/populate_location_data.py
+++ sagebrew/sb_locations/management/commands/populate_location_data.py
@@ -2,6 +2,8 @@ import os
 import us
 from json import loads, dumps
 
+from neomodel import DoesNotExist, db
+
 from django.core.management.base import BaseCommand
 
 from sb_locations.neo_models import Location
@@ -14,10 +16,12 @@ class Command(BaseCommand):
     def populate_location_data(self):
         try:
             usa = Location.nodes.get(name='United States of America')
-            usa.delete()
-        except Location.DoesNotExist:
-            pass
-        usa = Location(name="United States of America").save()
+        except (DoesNotExist, Location.DoesNotExist):
+            usa = Location(name="United States of America").save()
+        if not usa.positions.all():
+            pres = Position(name="President").save()
+            usa.positions.connect(pres)
+            pres.location.connect(usa)
         for root, dirs, files in \
                 os.walk('sb_locations/management/commands/states/'):
             if not dirs:
@@ -25,14 +29,18 @@ class Command(BaseCommand):
                 state_name = us.states.lookup(state).name
                 with open(root + "/" + files[0], 'r') as geo_data:
                     file_data = loads(geo_data.read())
-                    state = Location(name=state_name,
-                                     geo_data=dumps(
-                                         file_data['coordinates'])).save()
+                    try:
+                        state = Location.nodes.get(name=state_name)
+                    except (DoesNotExist, Location.DoesNotExist):
+                        state = Location(name=state_name,
+                                         geo_data=dumps(
+                                             file_data['coordinates'])).save()
                     usa.encompasses.connect(state)
                     state.encompassed_by.connect(usa)
-                    senator = Position(name='Senator').save()
-                    senator.location.connect(state)
-                    state.positions.connect(senator)
+                    if not state.positions.all():
+                        senator = Position(name='Senator').save()
+                        senator.location.connect(state)
+                        state.positions.connect(senator)
         for root, dirs, files in \
                 os.walk('sb_locations/management/commands/districts/'):
             if files[0] != '.DS_Store':
@@ -44,17 +52,24 @@ class Command(BaseCommand):
                     name=us.states.lookup(state).name)
                 with open(root + "/shape.geojson") as geo_data:
                     file_data = loads(geo_data.read())
-                    district = Location(name=int(district),
-                                        geo_data=dumps(
+                    query = 'MATCH (l:Location {name:"%s"})-[:ENCOMPASSES]->' \
+                            '(d:Location {name:"%s"}) RETURN d' % \
+                            (state_node.name, district)
+                    res, _ = db.cypher_query(query)
+                    if not res:
+                        district = Location(name=int(district),
+                                            geo_data=dumps(
                                             file_data['coordinates'])).save()
-                    district.encompassed_by.connect(state_node)
-                    district.encompassed_by.connect(usa)
-                    usa.encompasses.connect(district)
-                    state_node.encompasses.connect(district)
-                    house_rep = Position(name="House Representative").save()
-                    house_rep.location.connect(district)
-                    district.positions.connect(house_rep)
+                        district.encompassed_by.connect(state_node)
+                        district.encompassed_by.connect(usa)
+                        usa.encompasses.connect(district)
+                        state_node.encompasses.connect(district)
+                        if not district.positions.all():
+                            house_rep = Position(
+                                name="House Representative").save()
+                            house_rep.location.connect(district)
+                            district.positions.connect(house_rep)
         return True
 
     def handle(self, *args, **options):
-        return self.populate_location_data()
+        self.populate_location_data()

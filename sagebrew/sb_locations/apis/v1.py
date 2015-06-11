27decff7c08a7d06df639e141b3b4ec974d676e0
tylerwiersing <tyler.wiersing@gmail.com>
2015-06-11 18:17:26 -0400

    rep registration

--- sagebrew/sb_locations/apis/v1.py
+++ sagebrew/sb_locations/apis/v1.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 
 from rest_framework import routers
 
-from sb_locations.endpoints import LocationList
+from sb_locations.endpoints import LocationList, render_positions
 
 router = routers.SimpleRouter()
 
@@ -11,4 +11,5 @@ router.register(r'locations', LocationList, base_name='location')
 urlpatterns = patterns(
     'sb_locations.endpoints',
     url(r'^', include(router.urls)),
+    url(r'^(?P<name>[A-Za-z]{1,40})/comments/render/$', render_positions),
 )

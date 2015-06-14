4b9354b4d1bd8d82a1edac009813b8b2406b958c
Devon Bleibtrey <bleib1dj@gmail.com>
2015-06-14 11:48:54 -0400

    Updating cloudfront access

--- sagebrew/sagebrew/settings/staging.py
+++ sagebrew/sagebrew/settings/staging.py
@@ -3,6 +3,7 @@ from production import *
 ALLOWED_HOSTS = ['staging.sagebrew.com', ]
 
 S3_URL = "https://d3bfx8awejj0jg.cloudfront.net/"
+AWS_S3_CUSTOM_DOMAIN = "d3bfx8awejj0jg.cloudfront.net"
 WEB_ADDRESS = "https://staging.sagebrew.com"
 EMAIL_VERIFICATION_URL = "%s/registration/email_confirmation/" % WEB_ADDRESS
 BROKER_TRANSPORT_OPTIONS["polling_interval"] = 30

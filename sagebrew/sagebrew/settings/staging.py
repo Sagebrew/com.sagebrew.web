ca9896baede93f1e9a324788b9031a99c903188d
Devon Bleibtrey <bleib1dj@gmail.com>
2015-06-13 20:19:09 -0400

    Trying cloudfront drop in

--- sagebrew/sagebrew/settings/staging.py
+++ sagebrew/sagebrew/settings/staging.py
@@ -2,6 +2,7 @@ from production import *
 
 ALLOWED_HOSTS = ['staging.sagebrew.com', ]
 
+S3_URL = "https://d3bfx8awejj0jg.cloudfront.net/"
 WEB_ADDRESS = "https://staging.sagebrew.com"
 EMAIL_VERIFICATION_URL = "%s/registration/email_confirmation/" % WEB_ADDRESS
 BROKER_TRANSPORT_OPTIONS["polling_interval"] = 30

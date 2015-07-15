f288e8d32ed90e0ebc49d4c62f19a24b2ba00add
tylerwiersing <tyler.wiersing@gmail.com>
2015-07-15 18:23:46 -0400

    WA-1384 #time 20m

--- sagebrew/plebs/static/js/get_possible_helpers.js
+++ sagebrew/plebs/static/js/get_possible_helpers.js
@@ -1,15 +1,97 @@
 /*global $, jQuery, ajaxSecurity*/
+function enablePromotion(campaignId) {
+    $(".js-add_accountant").click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            xhrFields: {withCredentials: true},
+            type: "POST",
+            url: "/v1/campaigns/" + campaignId + "/add_accountants/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $("#js-accountant_wrapper").append(data);
+            },
+            error: function (XMLHttpRequest) {
+                if (XMLHttpRequest.status === 500) {
+                    $("#server_error").show();
+                }
+            }
+        });
+    });
+    $(".js-add_editor").click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            xhrFields: {withCredentials: true},
+            type: "POST",
+            url: "/v1/campaigns/" + campaignId + "/add_editors/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $("#js-accountant_wrapper").append(data);
+            },
+            error: function (XMLHttpRequest) {
+                if (XMLHttpRequest.status === 500) {
+                    $("#server_error").show();
+                }
+            }
+        });
+    });
+}
+
+function enableEditorRemoval(campaignId) {
+    $(".js-remove_editor").click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            xhrFields: {withCredentials: true},
+            type: "POST",
+            url: "/v1/campaigns/" + campaignId + "/add_accountants/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $("#js-accountant_wrapper").append(data);
+            },
+            error: function (XMLHttpRequest) {
+                if (XMLHttpRequest.status === 500) {
+                    $("#server_error").show();
+                }
+            }
+        });
+    });
+}
+
+function enableAccountantRemoval(campaignId) {
+    $(".js-remove_accountant").click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            xhrFields: {withCredentials: true},
+            type: "POST",
+            url: "/v1/campaigns/" + campaignId + "/add_accountants/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $("#js-accountant_wrapper").append(data);
+            },
+            error: function (XMLHttpRequest) {
+                if (XMLHttpRequest.status === 500) {
+                    $("#server_error").show();
+                }
+            }
+        });
+    });
+}
+
 $(document).ready(function () {
-    var campaign_id = $("#js-campaign_id").data('object_uuid'),
+    var campaignId = $("#js-campaign_id").data('object_uuid'),
         username = $("#js-campaign_id").data('username');
     $.ajax({
         xhrFields: {withCredentials: true},
         type: "GET",
-        url: "/v1/campaigns/" + campaign_id + "/possible_helpers/?html=true",
+        url: "/v1/campaigns/" + campaignId + "/possible_helpers/?html=true",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data) {
             $("#js-quest_helper_wrapper").append(data);
+            enablePromotion(campaignId);
         },
         error: function (XMLHttpRequest) {
             if (XMLHttpRequest.status === 500) {
@@ -20,11 +102,12 @@ $(document).ready(function () {
     $.ajax({
         xhrFields: {withCredentials: true},
         type: "GET",
-        url: "/v1/campaigns/" + campaign_id + "/accountants/?html=true",
+        url: "/v1/campaigns/" + campaignId + "/accountants/?html=true",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data) {
             $("#js-accountant_wrapper").append(data);
+            enableAccountantRemoval(campaignId);
         },
         error: function (XMLHttpRequest) {
             if (XMLHttpRequest.status === 500) {
@@ -35,11 +118,12 @@ $(document).ready(function () {
     $.ajax({
         xhrFields: {withCredentials: true},
         type: "GET",
-        url: "/v1/campaigns/" + campaign_id + "/editors/?html=true",
+        url: "/v1/campaigns/" + campaignId + "/editors/?html=true",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data) {
             $("#js-editor_wrapper").append(data);
+            enableEditorRemoval(campaignId);
         },
         error: function (XMLHttpRequest) {
             if (XMLHttpRequest.status === 500) {

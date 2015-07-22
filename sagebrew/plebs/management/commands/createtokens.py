2384ec6eaacc1177a5486427ec9974d9fb1fe27a
Devon Bleibtrey <bleib1dj@gmail.com>
2015-07-22 17:29:41 -0400

    Fixing sys util deployment

--- sagebrew/plebs/management/commands/createtokens.py
+++ sagebrew/plebs/management/commands/createtokens.py
@@ -7,17 +7,8 @@ from rest_framework.authtoken.models import Token
 class Command(BaseCommand):
 
     def create_tokens(self):
-        """
-        This function accesses the local file university_list.csv, converts
-        it into a
-        dictionary then creates a node of University with the information in
-        each dictionary
-
-        :return:
-        """
-
-    for user in User.objects.all():
-        Token.objects.get_or_create(user=user)
+        for user in User.objects.all():
+            Token.objects.get_or_create(user=user)
 
     def handle(self, *args, **options):
         self.create_tokens()

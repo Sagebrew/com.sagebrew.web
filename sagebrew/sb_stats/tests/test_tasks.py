0d1e684860ab327f78f203eba83d69c32c332f16
tylerwiersing <tyler.wiersing@gmail.com>
2015-07-30 17:10:42 -0400

    more tests

---
+++ sagebrew/sb_stats/tests/test_tasks.py
@@ -0,0 +1,48 @@
+import time
+
+from django.conf import settings
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from plebs.neo_models import Pleb
+from sb_questions.neo_models import Question
+from sb_registration.utils import create_user_util_test
+
+from sb_stats.tasks import update_view_count_task
+
+
+class TestUpdateViewCountTask(TestCase):
+    def setUp(self):
+        self.email = "success@simulator.amazonses.com"
+        res = create_user_util_test(self.email)
+        self.username = res["username"]
+        self.assertNotEqual(res, False)
+        self.pleb = Pleb.nodes.get(email=self.email)
+        self.user = User.objects.get(email=self.email)
+        self.question = Question().save()
+        settings.CELERY_ALWAYS_EAGER = True
+
+    def tearDown(self):
+        settings.CELERY_ALWAYS_EAGER = False
+
+    def test_update_view_count_task(self):
+        data = {
+            "username": self.pleb.username,
+            "object_uuid": self.question.object_uuid
+        }
+        res = update_view_count_task.apply_async(kwargs=data)
+        while not res.ready():
+            time.sleep(1)
+        self.assertTrue(res.result)
+
+    def test_update_view_count_task_already_connected(self):
+        self.question.viewed_by.connect(self.pleb)
+        self.pleb.viewed.connect(self.question)
+        data = {
+            "username": self.pleb.username,
+            "object_uuid": self.question.object_uuid
+        }
+        res = update_view_count_task.apply_async(kwargs=data)
+        while not res.ready():
+            time.sleep(1)
+        self.assertTrue(res.result)

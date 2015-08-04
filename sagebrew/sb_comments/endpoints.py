c43395379aaa334818dfaabe6d19e63e3797b948
tylerwiersing <tyler.wiersing@gmail.com>
2015-08-04 16:23:27 -0400

    flake8

--- sagebrew/sb_comments/endpoints.py
+++ sagebrew/sb_comments/endpoints.py
@@ -6,7 +6,8 @@ from django.template import RequestContext
 
 from rest_framework.reverse import reverse
 from rest_framework.decorators import (api_view, permission_classes)
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import (ListCreateAPIView)

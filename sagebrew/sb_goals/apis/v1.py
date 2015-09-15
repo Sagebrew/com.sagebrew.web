f5e284fe9c7dade8586c1cbbbcd137222ca79de7
Devon Bleibtrey <bleib1dj@gmail.com>
2015-09-14 22:05:53 -0400

    Reverting change to sb_goals until Tyler has a look

--- sagebrew/sb_goals/apis/v1.py
+++ sagebrew/sb_goals/apis/v1.py
@@ -12,8 +12,8 @@ router.register(r'goals', GoalRetrieveUpdateDestroy, base_name='goal')
 urlpatterns = patterns(
     'sb_goals.endpoints',
     url(r'^', include(router.urls)),
-    url(r'^rounds/(?P<object_uuid>[A-Za-z0-9.@_%+-]{2,36})/$',
+    url(r'^rounds/(?P<object_uuid>[A-Za-z0-9.@_%+-]{36,36})/$',
         RoundRetrieve.as_view(), name="round-detail"),
-    url(r'^rounds/(?P<object_uuid>[A-Za-z0-9.@_%+-]{2,36})/render_goals/$',
+    url(r'^rounds/(?P<object_uuid>[A-Za-z0-9.@_%+-]{36,36})/render_goals/$',
         render_round_goals, name="round-goal-render"),
 )

c2983b55c97902cdb291efc031d18796ecbe025e
Devon Bleibtrey <bleib1dj@gmail.com>
2015-09-18 21:46:22 -0400

    Reverting mistype

--- sagebrew/sb_updates/urls.py
+++ sagebrew/sb_updates/urls.py
@@ -6,5 +6,5 @@ from .views import edit_update
 urlpatterns = patterns(
     'sb_updates.views',
     url(r'^(?P<object_uuid>[A-Za-z0-9.@_%+-]{1,36})/edit/$',
-        edit_update, name='quest_updates')
+        edit_update, name='update-edit')
 )

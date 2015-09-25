843635f53f96d1f7ad642ee1720ee53c331a6061
tylerwiersing <tyler.wiersing@gmail.com>
2015-09-25 19:24:13 -0400

    tests

--- sagebrew/plebs/tests/test_neo_models.py
+++ sagebrew/plebs/tests/test_neo_models.py
@@ -98,6 +98,7 @@ class TestAddress(TestCase):
         except (Location.DoesNotExist, DoesNotExist):
             self.state = Location(name="Michigan").save()
 
+
     def test_set_encompassing_no_nodes(self):
         res = self.address.set_encompassing()
         city = Location.nodes.get(name="Wixom")
@@ -111,3 +112,22 @@ class TestAddress(TestCase):
         res = self.address.set_encompassing()
         self.assertTrue(res.encompassed_by.is_connected(city))
         city.delete()
+
+    def test_multiple_cities_same_name(self):
+        address = Address(city="Redford", state="Michigan").save()
+        city = Location(name="Redford").save()
+        city2 = Location(name="Redford").save()
+        try:
+            new_state = Location.nodes.get(name="Washington")
+        except (Location.DoesNotExist, DoesNotExist):
+            new_state = Location(name="Washington").save()
+        self.state.encompasses.connect(city)
+        city.encompassed_by.connect(self.state)
+        city2.encompassed_by.connect(new_state)
+
+        new_state.encompasses.connect(city2)
+        res = address.set_encompassing()
+        self.assertTrue(res.encompassed_by.is_connected(city))
+        city.delete()
+        city2.delete()
+        address.delete()

221450728be436ed089a1fa92e00fdef729848ed
tylerwiersing <tyler.wiersing@gmail.com>
2015-09-29 17:33:50 -0400

    management command update

--- sagebrew/sb_locations/management/commands/add_cities_from_addresses.py
+++ sagebrew/sb_locations/management/commands/add_cities_from_addresses.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 
+from neomodel import DoesNotExist
+
 from plebs.neo_models import Address
 from sb_locations.neo_models import Location
 
@@ -8,13 +10,15 @@ class Command(BaseCommand):
     args = "None."
 
     def add_cities_from_addresses(self):
-        wixom = Location.nodes.get(
-            object_uuid="3ed3dfe8-5bc4-11e5-8dd9-0242ac110003")
-        for location in wixom.encompassed_by.all():
-            if location.name == "11":
-                wixom.encompassed_by.disconnect(location)
-                location.encompasses.disconnect(wixom)
-
+        try:
+            wixom = Location.nodes.get(
+                object_uuid="3ed3dfe8-5bc4-11e5-8dd9-0242ac110003")
+            for location in wixom.encompassed_by.all():
+                if location.name == "11":
+                    wixom.encompassed_by.disconnect(location)
+                    location.encompasses.disconnect(wixom)
+        except (DoesNotExist, Location.DoesNotExist):
+            pass
         for address in Address.nodes.all():
             address.set_encompassing()
 

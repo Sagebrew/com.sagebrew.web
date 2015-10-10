5f8443f65a64910e303e605d01b0ed97b58c6534
Matt Wisner <matt@detroitweb.company>
2015-10-10 16:43:58 -0400

    Gulp build improvements

--- sagebrew/sagebrew/static/gulpfile.js
+++ sagebrew/sagebrew/static/gulpfile.js
@@ -1,22 +1,25 @@
-//
-// Load various glup plugins.
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    less = require('gulp-less'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    minifycss = require('gulp-minify-css'),
-    embedlr = require('gulp-embedlr'),
-    clean = require('gulp-clean'),
-    refresh = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr(),
-    notify = require('gulp-notify');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var minifycss = require('gulp-minify-css');
+var embedlr = require('gulp-embedlr');
+var del = require('del');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
+var notify = require('gulp-notify');
 
 //
 // Path definitions.
 // Most setups don't separate vendor scripts from app scripts. But we're going to do it anyway.
 // TODO: Cleanup file system so that we can use entire folders.
+// TOOO: Merge scripts, scripts-anon, scripts-auth into one.
 var paths = {
     vendor_scripts: [
         'js/vendor/jquery-2.1.4.min.js',
@@ -36,7 +39,7 @@ var paths = {
         'js/sign_up_btn.js'
     ],
     styles: [
-        'less/**'
+        'less/**/*.less'
     ],
     fonts: [
         'css/vendor/font-awesome-4.3.0/fonts/*',
@@ -52,31 +55,11 @@ var paths = {
     ]
 };
 
-//
-// JS
-gulp.task('scripts', function () {
-    'use strict';
-    gulp.src(paths.vendor_scripts)
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('dist/build'))
-        .pipe(refresh(server));
-
-    gulp.src(paths.scripts)
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(concat('sagebrew.js'))
-        .pipe(gulp.dest('dist/build'))
-        .pipe(refresh(server));
-});
 
 //
-// Styles
-gulp.task('styles', function () {
-    gulp.src(['less/styles.less'])
-        .pipe(less())
-        .pipe(minifycss())
-        .pipe(gulp.dest('dist/build'))
-        .pipe(refresh(server));
+// Clean
+gulp.task('clean', function() {
+  return del(['dist']);
 });
 
 //
@@ -89,42 +72,74 @@ gulp.task('lr-server', function() {
 });
 
 //
+// JS
+gulp.task('scripts:app', ['clean'], function () {
+    return gulp.src(paths.scripts)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(concat('sagebrew.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(refresh(server));
+
+});
+
+//
+// JS
+gulp.task('scripts:vendor', ['clean'], function () {
+    return gulp.src(paths.vendor_scripts)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(refresh(server));
+});
+
+//
+// JS
+gulp.task('scripts', ['scripts:vendor', 'scripts:app']);
+
+//
+// Styles
+gulp.task('styles', ['clean'], function () {
+    return gulp.src(['less/styles.less'])
+        .pipe(less())
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(refresh(server));
+});
+
+
+//
 // Fonts
-gulp.task('fonts', function() {
-    gulp.src(paths.fonts)
+gulp.task('fonts', ['clean'], function() {
+    return gulp.src(paths.fonts)
             .pipe(gulp.dest('dist/fonts/'));
+});
+
+
+//
+// Hotfix for lightbox images.
+// TODO: Fix.
+gulp.task('images:hotfix', ['clean'], function() {
+    return gulp.src(['css/vendor/img/**'])
+           .pipe(gulp.dest('dist/css/vendor/img/'));
 });
 
 //
 // Images
-gulp.task('images', function() {
-    gulp.src(paths.images)
+gulp.task('images', ['clean', 'images:hotfix'], function() {
+    return gulp.src(paths.images)
             .pipe(gulp.dest('dist/imgs/'));
-
-    //Bad fix for lightbox.
-    //TODO: Stop This...
-    gulp.src(['css/vendor/img/**'])
-            .pipe(gulp.dest('dist/css/vendor/img/'));
-});
-
-//
-// Clean
-// TODO: Fix this, it throws an error.
-gulp.task('clean', function () {
-    gulp.src(['dist/build', 'dist/fonts', 'dist/imgs'], { read: false })
-        .pipe(clean());
 });
 
 //
 // Default task.
-gulp.task('default', function () {
+gulp.task('watch', function () {
     'use strict';
 
-    var client = ['lr-server', 'styles', 'scripts', 'fonts', 'images'];
-    gulp.run(client);
-
-    //TODO: Only rebuild styles when styles are edited, only rebuild scripts when scripts are edited.
-    gulp.watch(paths.styles, client);
-    gulp.watch(paths.scripts, client);
+    gulp.watch(paths.styles, ['styles']);
+    gulp.watch(paths.scripts, ['scripts']);
 
 });
+
+//
+// Default task.
+gulp.task('default', ['watch', 'scripts', 'styles', 'images', 'fonts']);

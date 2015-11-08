45bb470c35dec5c317e8721eac0c628882175e03
tylerwiersing <tyler.wiersing@gmail.com>
2015-11-07 21:17:21 -0500

    WA-1552 #time 5m #done created management command to migrate friend relationship over from relying on currently_friends to rely on activve

---
+++ sagebrew/plebs/management/commands/migrate_friend_relationship.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from neomodel import db
+
+
+class Command(BaseCommand):
+    args = "None."
+
+    def migrate_friend_relationship(self):
+        query = 'MATCH (p:Pleb)-[r:FRIENDS_WITH]->(p2:Pleb) ' \
+                'FOREACH (rel in [r]|SET rel.active = rel.currently_friends)' \
+                ' RETURN r'
+        res, _ = db.cypher_query(query)
+        print res
+        return True
+
+    def handle(self, *args, **options):
+        self.migrate_friend_relationship()

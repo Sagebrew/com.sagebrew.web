9217b96a8edace508ca56345ba4066ec92193c4f
tylerwiersing <tyler.wiersing@gmail.com>
2015-11-24 13:41:48 -0500

    tests

--- sagebrew/sb_uploads/tests/test_endpoints.py
+++ sagebrew/sb_uploads/tests/test_endpoints.py
@@ -264,7 +264,7 @@ class URLContentEndpointTests(APITestCase):
         }
         response = self.client.post(url, data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('https://sagebrew-dev.s3.amazonaws.com/media/',
+        self.assertIn('.s3.amazonaws.com/media/',
                       response.data['selected_image'])
 
     def test_create_not_og(self):

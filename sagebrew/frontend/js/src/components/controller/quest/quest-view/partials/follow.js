0d189915445550dacbc2e570985acbb03257d61d
Devon Bleibtrey <bleib1dj@gmail.com>
2016-01-14 21:55:51 -0500

    Styling updates WA-1689 #done WA-1690 #done WA-1688 #done WA-1687 #done

--- sagebrew/frontend/js/src/components/controller/quest/quest-view/partials/follow.js
+++ sagebrew/frontend/js/src/components/controller/quest/quest-view/partials/follow.js
@@ -2,20 +2,22 @@ var helpers = require('common/helpers'),
     request = require('api').request;
 
 export function load() {
-    var followButton = $(".js-follow-btn"),
+    var followButton = document.getElementById('js-follow-btn'),
         app = $(".app-sb"),
         followText,
         questId;
 
-    app.on('click', ".js-follow-btn", function() {
-        followText = followButton.text().toLowerCase();
+    app.on('click', "#js-follow-btn", function() {
+        followText = followButton.innerText.toLowerCase();
         questId = helpers.args(1);
+        followButton.disabled = true;
         request.post({url:"/v1/quests/" + questId + "/" + followText + "/"})
             .done(function() {
+                followButton.disabled = false;
                 if (followText === "follow") {
-                    followButton.text("Unfollow");
+                    followButton.innerText = "Unfollow";
                 } else {
-                    followButton.text("Follow");
+                    followButton.innerText = "Follow";
                 }
             });
     });

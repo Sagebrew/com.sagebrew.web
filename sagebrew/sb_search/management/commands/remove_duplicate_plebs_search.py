8b8733f35894cbfd3dcaa4687d10fbbad8c36d6b
tylerwiersing <tyler.wiersing@gmail.com>
2016-01-18 14:25:42 -0500

    new management command to remove duplicate plebs from search

--- sagebrew/sb_search/management/commands/remove_duplicate_plebs_search.py
+++ sagebrew/sb_search/management/commands/remove_duplicate_plebs_search.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
 
 from plebs.neo_models import Pleb
-from plebs.serializers import PlebSerializerNeo
 
 
 class Command(BaseCommand):
@@ -13,8 +13,11 @@ class Command(BaseCommand):
     def update_pleb_search(self):
         es = Elasticsearch(settings.ELASTIC_SEARCH_HOST)
         for pleb in Pleb.nodes.all():
-            es.index(index='full-search-base', doc_type='profile',
-                     id=pleb.object_uuid, body=PlebSerializerNeo(pleb).data)
+            try:
+                es.delete(index='full-search-base', doc_type='profile',
+                          id=pleb.username)
+            except NotFoundError:
+                pass
 
     def handle(self, *args, **options):
         self.update_pleb_search()

47a09abc00b3acff6a24a30cc204ab8b507d3c00
Devon Bleibtrey <bleib1dj@gmail.com>
2016-01-31 00:06:29 -0500

    Fixing search index

--- sagebrew/plebs/management/commands/remove_duplicates_from_search.py
+++ sagebrew/plebs/management/commands/remove_duplicates_from_search.py
@@ -1,7 +1,11 @@
-from django.conf import settings
+# from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from elasticsearch import Elasticsearch, NotFoundError
+# from elasticsearch import Elasticsearch, NotFoundError
+
+from api.utils import spawn_task
+from sb_search.tasks import update_search_object
+from plebs.neo_models import Pleb
 
 
 class Command(BaseCommand):
@@ -9,7 +13,17 @@ class Command(BaseCommand):
     help = 'Remove duplicate Tyler in Search'
 
     def remove_duplicate(self):
-        es = Elasticsearch(settings.ELASTIC_SEARCH_HOST)
+        # es = Elasticsearch(settings.ELASTIC_SEARCH_HOST)
+        pleb = Pleb.nodes.get(username="robin_branch")
+        task_data = {
+            "object_uuid": pleb.object_uuid,
+            "instance": pleb
+        }
+        spawn_task(
+            task_func=update_search_object,
+            task_param=task_data,
+            countdown=30)
+        """
         try:
             es.delete(index="full-search-base", id="devon_bleibtrey",
                       doc_type="quest")
@@ -25,6 +39,7 @@ class Command(BaseCommand):
                       doc_type="politicalcampaign")
         except NotFoundError:
             pass
+        """
 
     def handle(self, *args, **options):
         self.remove_duplicate()

48d6ba069903c8e873d927b4f6ee60b5b28d3167
Devon Bleibtrey <bleib1dj@gmail.com>
2016-02-02 15:55:21 -0500

    Pep8

--- sagebrew/plebs/management/commands/remove_duplicates_from_search.py
+++ sagebrew/plebs/management/commands/remove_duplicates_from_search.py
@@ -3,8 +3,6 @@ from django.core.management.base import BaseCommand
 
 from elasticsearch import Elasticsearch, NotFoundError
 
-from plebs.neo_models import Pleb
-
 
 class Command(BaseCommand):
     args = 'None.'

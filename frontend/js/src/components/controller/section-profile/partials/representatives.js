dde62115c5bc82e93f33b9c28e754e6ec7de36c2
Devon Bleibtrey <bleib1dj@gmail.com>
2016-03-10 18:02:57 -0500

    Cleaning up representative partial

--- frontend/js/src/components/controller/section-profile/partials/representatives.js
+++ frontend/js/src/components/controller/section-profile/partials/representatives.js
@@ -19,13 +19,9 @@ export function init() {
     if ($("#president_wrapper").length) {
         var rcp = request.get({url: '/v1/profiles/' + pageUser + '/president/'}),
             rcs = request.get({url: '/v1/profiles/' + pageUser + '/senators/'}),
-            rchr = request.get({url: '/v1/profiles/' + pageUser + '/house_representative/'}),
-            rps = request.get({url: '/v1/profiles/' + pageUser + '/possible_senators/?html=true'}),
-            rphr = request.get({url: '/v1/profiles/' + pageUser + '/possible_house_representatives/?html=true'}),
-            rpp = request.get({url: '/v1/profiles/' + pageUser + '/possible_presidents/?html=true'}),
-            rplr = request.get({url: '/v1/profiles/' + pageUser + '/possible_local_representatives/?html=true'});
+            rchr = request.get({url: '/v1/profiles/' + pageUser + '/house_representative/'});
 
-        $.when(rcp, rcs, rchr, rps, rphr, rpp, rplr).done(function (dcp, dcs, dchr, dps, dphr, dpp, dplr) {
+        $.when(rcp, rcs, rchr).done(function (dcp, dcs, dchr) {
             dcp[0].terms = helpers.getOrdinal(dcp[0].terms);
             dcp[0].has_website = !!dcp[0].quest.website;
             $("#president_wrapper").append(sittingRepTemplate({"representative": [dcp[0]]}));
@@ -39,11 +35,6 @@ export function init() {
             dchr[0].terms = helpers.getOrdinal(dchr[0].terms);
             dchr[0].has_website = !!dchr[0].quest.website;
             $("#house_rep_wrapper").append(sittingRepTemplate({"representative": [dchr[0]]}));
-
-            $("#potential_senator_wrapper").append(dps[0]);
-            $("#potential_rep_wrapper").append(dphr[0]);
-            $("#potential_president_wrapper").append(dpp[0]);
-            $("#potential_local_wrapper").append(dplr[0]);
         });
     }
 }

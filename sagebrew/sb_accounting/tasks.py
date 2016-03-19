725b6e4b9f46cd5c9216acbf4930ec396b346bef
tylerwiersing <tyler.wiersing@gmail.com>
2016-03-19 19:28:30 -0400

    updated query for unverified quests

--- sagebrew/sb_accounting/tasks.py
+++ sagebrew/sb_accounting/tasks.py
@@ -8,8 +8,6 @@ from django.conf import settings
 
 from neomodel import db
 
-from sb_quests.neo_models import Quest
-
 from logging import getLogger
 logger = getLogger('loggly_logs')
 
@@ -20,16 +18,17 @@ def check_unverified_quest():
     Intercom.app_api_key = settings.INTERCOM_API_KEY
     now = datetime.now(pytz.utc)
     query = 'MATCH (q:Quest) WHERE q.account_verified<>"verified" ' \
-            'AND q.account_first_updated IS NOT NULL RETURN q'
+            'AND q.account_first_updated IS NOT NULL AND ' \
+            '%d-q.account_first_updated >= 86400 RETURN ' \
+            'q.owner_username as username' % \
+            (calendar.timegm(now.utctimetuple()))
     res, _ = db.cypher_query(query)
     for row in res:
-        quest = Quest.inflate(row[0])
-        if (now - quest.account_first_updated).days >= 1:
-            try:
-                Event.create(event_name="still-unverified-quest",
-                             user_id=quest.owner_username,
-                             created=calendar.timegm(now.utctimetuple()))
-            except errors.ResourceNotFound:
-                continue
+        try:
+            Event.create(event_name="still-unverified-quest",
+                         user_id=row.username,
+                         created=calendar.timegm(now.utctimetuple()))
+        except errors.ResourceNotFound:
+            continue
     logger.critical("Scheduled unverified Quest task ran!")
     return True

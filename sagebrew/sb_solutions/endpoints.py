54830910b601b432b8939a4dfcd717f678da25c4
tylerwiersing <tyler.wiersing@gmail.com>
2016-04-16 13:20:01 -0400

    more logging for search tasks, removing custom defined solution list call

--- sagebrew/sb_solutions/endpoints.py
+++ sagebrew/sb_solutions/endpoints.py
@@ -93,12 +93,6 @@ class ObjectSolutionsListCreate(ListCreateAPIView):
             Solution, query=query, distinct=True, descending=True)\
             .order_by(reduce_query)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        serializer = self.get_serializer(page, many=True)
-        return self.get_paginated_response(serializer.data)
-
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data,
                                          context={"request": request})

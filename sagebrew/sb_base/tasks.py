661ac79a58b93ee9340ad34b5a5bb3d61392bab5
Devon Bleibtrey <bleib1dj@gmail.com>
2016-06-03 23:17:14 -0400

    Fixing test

--- sagebrew/sb_base/tasks.py
+++ sagebrew/sb_base/tasks.py
@@ -18,7 +18,13 @@ def create_email(message_data):
         Message.create(**message_data)
     except (ResourceNotFound, UnexpectedError) as e:
         raise create_email.retry(exc=e, countdown=10, max_retries=None)
-    except (RateLimitExceeded, ServerError, ServiceUnavailableError,
+    except RateLimitExceeded as e:
+        # We don't want to continue bashing their API if we've
+        # hit our limit. Intercom may then reduce our overall limit as
+        # highlighted here
+        # https://developers.intercom.io/reference#rate-limiting
+        raise create_email.retry(exc=e, countdown=3600, max_retries=None)
+    except (ServerError, ServiceUnavailableError,
             BadGatewayError, HttpError,
             AuthenticationError) as e:  # pragma: no cover
         # Not covering because Intercom does not have a good way of
@@ -40,7 +46,16 @@ def create_event(event_name, username, metadata=None):
             metadata=metadata)
     except (ResourceNotFound, UnexpectedError) as e:
         raise create_event.retry(exc=e, countdown=10, max_retries=None)
-    except (RateLimitExceeded, ServerError, ServiceUnavailableError,
+    except RateLimitExceeded as e:
+        # We don't care too much about events getting spanned. They at least
+        # don't affect the customer in a highly noticeable way. So if we've
+        # exceeded our rate limit we can delay
+        # But we def don't want to continue bashing their API if we've
+        # hit our limit. Intercom may then reduce our overall limit as
+        # highlighted here
+        # https://developers.intercom.io/reference#rate-limiting
+        raise create_email.retry(exc=e, countdown=86400, max_retries=None)
+    except (ServerError, ServiceUnavailableError,
             BadGatewayError, HttpError,
             AuthenticationError) as e:  # pragma: no cover
         # Not covering because Intercom does not have a good way of

339c693d31749d9f193ed43a38b54946bed18117
Devon Bleibtrey <bleib1dj@gmail.com>
2016-06-03 20:14:22 -0400

    Adding cache check to onboard setup task

--- sagebrew/sb_missions/management/commands/setup_onboarding_for_missions.py
+++ sagebrew/sb_missions/management/commands/setup_onboarding_for_missions.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.core.cache import cache
+
 
 from sb_missions.neo_models import Mission
 from sb_missions.utils import setup_onboarding
@@ -11,6 +13,8 @@ class Command(BaseCommand):
         for mission in Mission.nodes.all():
             quest = Mission.get_quest(mission.object_uuid)
             setup_onboarding(quest, mission)
+        cache.set('onboard_setup', True)
 
     def handle(self, *args, **options):
-        self.setup_onboarding_retroactive()
+        if not cache.get('onboard_setup'):
+            self.setup_onboarding_retroactive()

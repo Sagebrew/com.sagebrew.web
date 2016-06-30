f2f4dbf513de70dba769bee6a28b6b269b11ad15
tylerwiersing <tyler.wiersing@gmail.com>
2016-06-30 17:43:06 -0400

    removing unused declarations

--- sagebrew/sb_missions/serializers.py
+++ sagebrew/sb_missions/serializers.py
@@ -120,10 +120,6 @@ class MissionSerializer(SBSerializer):
                 focused_on).title().replace('-', ' ').replace('_', ' ')
         district = validated_data.get('district')
         formatted_location_name = validated_data.get('formatted_location_name')
-        formatted_location_district = None
-        if district is not None:
-            formatted_location_district = formatted_location_name \
-                                          + ", District " + district
         # TODO what happens if a moderator makes the mission?
         owner_username = request.user.username
         title = focused_on.title().replace('-', ' ').replace('_', ' ')
@@ -133,9 +129,7 @@ class MissionSerializer(SBSerializer):
                           title=title,
                           wallpaper_pic=static(
                               'images/wallpaper_capitol_2.jpg'),
-                          formatted_location_name=formatted_location_name,
-                          formatted_district_name=
-                          formatted_location_district)\
+                          formatted_location_name=formatted_location_name)\
             .save()
         if focus_type == "position":
             if level == "federal":
